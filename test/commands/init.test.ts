6a2f94ec6fbfa8db7729f2dccf450479438383b9
David Thor <david.thor@arcussolutions.com>
2018-10-09 20:48:04 -0400

    Updated test script

--- test/commands/init.test.ts
+++ test/commands/init.test.ts
@@ -1,17 +1,65 @@
-import {expect, test} from '@oclif/test'
+import {expect, test} from '@oclif/test';
+import * as inquirer from 'inquirer';
+import * as sinon from 'sinon';
+
+interface InitInput {
+  name?: string;
+  version?: string;
+  description?: string;
+  keywords?: string[];
+  author?: string[];
+  license?: string;
+}
+
+const MOCK_SERVICE_CONFIG: InitInput = {
+  name: 'test-service',
+  version: '0.1.0',
+  description: 'Test description',
+  keywords: ['test', 'this'],
+  author: ['Architect'],
+  license: 'MIT'
+};
 
 describe('init', () => {
+  beforeEach(() => {
+    sinon.stub(inquirer, 'prompt')
+      .resolves((questions: inquirer.Question[]) => {
+        let mock_config: InitInput = MOCK_SERVICE_CONFIG;
+
+        questions.forEach((question: inquirer.Question) => {
+          if (
+            question.hasOwnProperty('default') &&
+            question.name !== undefined &&
+            Object.keys(mock_config).includes(question.name)
+          ) {
+            // @ts-ignore
+            mock_config[question.name] = question.default;
+          }
+        });
+
+        return mock_config;
+      });
+  });
+
+  afterEach(() => {
+    // @ts-ignore
+    inquirer.prompt.restore();
+  });
+
+
+  // START TESTS
   test
     .stdout()
     .command(['init'])
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
-    })
+    .it('should match default', ctx => {
+      // Check filesystem for config file
+      expect(ctx.stdout).to.contain('hello world');
+    });
 
   test
     .stdout()
     .command(['init', '--name', 'jeff'])
     .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
-    })
-})
+      expect(ctx.stdout).to.contain('hello jeff');
+    });
+});
