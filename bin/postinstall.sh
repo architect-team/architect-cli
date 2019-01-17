2bbd68b1e251e963d523950fc426a82f9c878790
David Thor <david.thor@arcussolutions.com>
2019-01-17 10:43:52 -0500

    Fixed order of grpc install during postinstall

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -5,6 +5,11 @@ ARCHITECT_PATH=$HOME/.architect
 mkdir -p ${ARCHITECT_PATH}
 grep -q -x -F 'export ARCHITECT_PATH=$ARCHITECT_PATH' ~/.bashrc || echo 'export ARCHITECT_PATH=$ARCHITECT_PATH' >> ~/.bashrc
 
+# Hack to allow GRPC to be used as part of the launcher and inside
+# the *_pb.js files
+npm install -g grpc
+npm link grpc
+
 # Check to see if GRPC and protoc has been installed
 protoc --version
 if [[ $? -ne 0 ]]; then
@@ -12,6 +17,9 @@ if [[ $? -ne 0 ]]; then
   git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc ${ARCHITECT_PATH}/grpc --recursive
   cd ${ARCHITECT_PATH}/grpc/
   make
+  cd ./third_party/protobuf
+  make install
+  cd ../../../
   protoc --version
   if [[ $? -ne 0 ]]; then
     echo "Unable to install protoc. Please follow instructions below to install GRPC from source:"
@@ -19,8 +27,3 @@ if [[ $? -ne 0 ]]; then
     exit 1
   fi
 fi
-
-# Hack to allow GRPC to be used as part of the launcher and inside
-# the *_pb.js files
-npm install -g grpc
-npm link grpc
