3792eae541b79909480123cf6035da3ccdd1bcbb
David Thor <david.thor@arcussolutions.com>
2019-01-17 11:46:12 -0500

    Fixed references to ARCHITECT_PATH in postinstall

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -3,7 +3,7 @@
 # Setup Architect home directory
 ARCHITECT_PATH=$HOME/.architect
 mkdir -p ${ARCHITECT_PATH}
-grep -q -x -F 'export ARCHITECT_PATH=$ARCHITECT_PATH' ~/.bashrc || echo 'export ARCHITECT_PATH=$ARCHITECT_PATH' >> ~/.bashrc
+grep -q -x -F "export ARCHITECT_PATH=${ARCHITECT_PATH}" ~/.bashrc || echo "export ARCHITECT_PATH=${ARCHITECT_PATH}" >> ~/.bashrc
 
 # Hack to allow GRPC to be used as part of the launcher and inside
 # the *_pb.js files
