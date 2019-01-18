926f5646495e059f5852e3b16abaabe7132e9841
David Thor <david.thor@arcussolutions.com>
2019-01-18 15:56:59 -0500

    Updated method for auto-installing protoc compiler to make it faster

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Set protoc version to install
+PROTOC_VERSION='3.6.1'
+PROTOC_DOWNLOAD_LINK='https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1'
+
 # Setup Architect home directory
 ARCHITECT_PATH=$HOME/.architect
 mkdir -p ${ARCHITECT_PATH}
@@ -13,6 +17,28 @@ npm link grpc --local
 # Check to see if GRPC and protoc has been installed
 protoc --version
 if [[ $? -ne 0 ]]; then
-  echo "Protoc compiler not installed. Please make sure to download the correct version for your OS:"
+  echo "Protoc compiler not found. Attempting to install."
+
+  # Detect system architecture
+  ARCHITECTURE=$(uname -m)
+  if [[ ${ARCHITECTURE} -ne 'x86_64' ]]; then
+    ARCHITECTURE='x86_32'
+  fi
+
+  # Detect operating system
+  OS='linux'
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS='osx'
+  elif [[ "$OSTYPE" == "win32" ]]; then
+    OS='win32'
+  fi
+
+  # Download protoc binary and add symlink in path
+  curl ${PROTOC_DOWNLOAD_LINK}/protoc-${PROTOC_VERSION}-${OS}-${ARCHITECTURE}.zip -L -o ${ARCHITECT_PATH}/protoc.zip
+  mkdir ${ARCHITECT_PATH}/protoc/
+  unzip ${ARCHITECT_PATH}/protoc.zip -d ${ARCHITECT_PATH}/protoc/
+  ln -s ${ARCHITECT_PATH}/protoc/bin/protoc /usr/local/bin/
+  protoc --version
+  echo "Unable to install protoc compiler. Please install manually:"
   echo "https://github.com/protocolbuffers/protobuf/releases"
 fi
