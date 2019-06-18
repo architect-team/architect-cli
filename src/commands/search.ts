64a16495b260ae63ed08c546050c5a3fc5106f7f
TJ Higgins <tj@architect.io>
2019-06-18 15:44:33 -0400

    Rename services:list to search

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,23 +1,21 @@
 import { flags } from '@oclif/command';
 import inquirer = require('inquirer');
 
-import Command from '../../base';
+import Command from '../base';
 
-export default class Services extends Command {
-  static description = 'List, create, or delete services';
-  static usage = 'services [OPTIONS]\n$ architect services:generate [ID] [OPTIONS]\n$ architect services:plan [ID] [OPTIONS]';
-  static aliases = ['services:list'];
+export default class Search extends Command {
+  static description = 'Search services';
 
   static args = [
     { name: 'service_id', description: 'Service Id', required: false }
   ];
 
   static flags = {
-    help: flags.help({ char: 'h' })
+    help: flags.help({ char: 'h' }),
   };
 
   async run() {
-    const { args } = this.parse(Services);
+    const { args } = this.parse(Search);
     inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
     const answers = await inquirer.prompt([{
