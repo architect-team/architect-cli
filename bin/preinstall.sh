bcd3314f040b58cbeed667ce1ad72b44a0e932d1
tjhiggins <tj@architect.io>
2019-08-12 10:30:32 -0400

    Add check for docker-compose and move to preinstall

--- bin/preinstall.sh
+++ bin/preinstall.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if ! command -v docker > /dev/null 2>&1; then
+  echoerr "Docker must be installed"
+  exit 1
+fi
+
+if ! command -v docker-compose > /dev/null 2>&1; then
+  echoerr "Docker compose must be installed"
+  exit 1
+fi
+
 case "$(uname -s)" in
   Linux)
     sudo apt-get update -y
