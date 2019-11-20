64dddc6e4f54215d37dabf57d8cc12825eab3a7e
David Thor <david.thor@arcussolutions.com>
2019-11-20 12:55:23 -0500

    Removed lib dir of graph library from version control

--- src/dependency-graph/lib/edge.d.ts
+++
@@ -1,9 +0,0 @@
-import { DependencyNode } from './node';
-declare type EDGE_TYPE = 'notification' | 'dependency';
-export default class DependencyEdge {
-    from: DependencyNode;
-    to: DependencyNode;
-    type: EDGE_TYPE;
-    constructor(from: DependencyNode, to: DependencyNode, type?: EDGE_TYPE);
-}
-export {};
--- src/dependency-graph/lib/node/datastore.d.ts
+++
@@ -1,4 +0,0 @@
-import { DependencyNodeOptions, DependencyNode } from '.';
-export declare class DatastoreNode extends DependencyNode {
-    constructor(options: DependencyNodeOptions);
-}
