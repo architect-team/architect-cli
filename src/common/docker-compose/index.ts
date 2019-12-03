fc4a79c2a618c27912e34b425269d012ae706a63
Ryan Cahill <ryan.cahill444@gmail.com>
2019-12-03 11:25:05 -0500

    wip

--- src/common/docker-compose/index.ts
+++ src/common/docker-compose/index.ts
@@ -13,6 +13,18 @@ export const generate = (dependency_manager: DependencyManager): DockerComposeTe
 
   // Enrich base service details
   dependency_manager.graph.nodes.forEach(node => {
+    const param_prefix = `$ARC_${node.name.replace('-', '_').replace('/', '_').replace('.', '_').toUpperCase()}_${node.tag.replace('.', '_').toUpperCase()}`;
+    const host_param_placeholder = `${param_prefix}_HOST`;
+    const port_param_placeholder = `${param_prefix}_PORT`; // how do we match these to other services?
+    const injected_params: { [key: string]: string } = {};
+    for (const [name, value] of Object.entries(node.parameters)) {
+      if (value === host_param_placeholder) {
+        injected_params[name] = `${node.protocol}${node.normalized_ref}`;
+      } else if (value === port_param_placeholder) {
+        injected_params[name] = node.ports.target.toString()
+      }
+    }
+
     compose.services[node.normalized_ref] = {
       ports: [`${node.ports.expose}:${node.ports.target}`],
       depends_on: [],
@@ -29,8 +41,10 @@ export const generate = (dependency_manager: DependencyManager): DockerComposeTe
         }),
         ARCHITECT_CURRENT_SERVICE: node.name,
         ...node.parameters,
+        ...injected_params,
       },
     };
+    //console.log(compose.services[node.normalized_ref])
 
     if (node instanceof ServiceNode || node instanceof LocalServiceNode) {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -81,6 +95,22 @@ export const generate = (dependency_manager: DependencyManager): DockerComposeTe
         port: edge.to.ports.target.toString(),
         api: edge.to.api.type,
       };
+
+      // check if param matches edge name for host or port
+      // service.environment[checked host or port]
+      const param_prefix = `$ARC_${edge.to.name.replace('-', '_').replace('/', '_').replace('.', '_').toUpperCase()}_${edge.to.tag.replace('.', '_').toUpperCase()}`;
+      const host_param_placeholder = `${param_prefix}_HOST`;
+      const port_param_placeholder = `${param_prefix}_PORT`; // how do we match these to other services?
+      const injected_params: { [key: string]: string } = {};
+      for (const [name, value] of Object.entries(edge.to.parameters)) {
+        if (value === host_param_placeholder) {
+          injected_params[name] = `${edge.to.protocol}${edge.to.normalized_ref}`;
+        } else if (value === port_param_placeholder) {
+          injected_params[name] = edge.to.ports.target.toString()
+        }
+      }
+      console.log(injected_params)
+
     }
 
     // Parse subscription logic
