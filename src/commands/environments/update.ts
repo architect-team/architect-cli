0f1b0680a63756ac88e1ac1ad4014c2e4e2b68bd
Ryan Cahill <ryan.cahill444@gmail.com>
2019-12-09 16:43:39 -0500

    fixing the update command

--- src/commands/environments/update.ts
+++ src/commands/environments/update.ts
@@ -1,10 +1,9 @@
-import {flags} from '@oclif/command';
-import Command from '../../base-command';
-import untildify from 'untildify';
-import inquirer from 'inquirer';
-import fs from 'fs-extra';
+import { flags } from '@oclif/command';
 import chalk from 'chalk';
-import { EnvironmentNameValidator } from '../../common/utils/validation';
+import fs from 'fs-extra';
+import inquirer from 'inquirer';
+import untildify from 'untildify';
+import Command from '../../base-command';
 import { readIfFile } from '../../common/utils/file';
 
 export default class EnvironmentUpdate extends Command {
@@ -31,29 +30,43 @@ export default class EnvironmentUpdate extends Command {
   };
 
   static args = [{
-    name: 'name',
+    name: 'environment',
     description: 'Name of the environment to update',
     parse: (value: string) => value.toLowerCase(),
+  }, {
+    name: 'account_name',
+    description: 'Account that the environment belongs to',
+    required: false
   }];
 
   async run() {
-    const {args, flags} = this.parse(EnvironmentUpdate)
+    const { args, flags } = this.parse(EnvironmentUpdate)
 
-    let answers: any = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        when: !args.name,
-        filter: value => value.toLowerCase(),
-        validate: value => {
-          if (EnvironmentNameValidator.test(value)) return true;
-          return `Name must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character`;
-        },
-      }
-    ]);
+    let answers: any = await inquirer.prompt([{
+      type: 'input',
+      name: 'account_name',
+      message: 'What account does the environment belong to?',
+      when: !args.account_name,
+    }]);
+
+    const { data: fetched_account } = await this.app.api.get(`/accounts/${answers.account_name || args.account_name.split('=')[1]}`);
+
+    inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+    answers = Object.assign({}, answers, await inquirer.prompt([{
+      type: 'autocomplete',
+      name: 'environment',
+      message: 'Select environment:',
+      source: async (_: any, input: string) => {
+        const params = { q: input };
+        const { data: environments } = await this.app.api.get(`/accounts/${fetched_account.id}/environments`, { params });
+        return environments.map((environment: any) => environment.name);
+      },
+      when: !args.environment,
+    }]));
+
     answers = { ...args, ...flags, ...answers };
-
-    await this.app.api.put(`/environments/${answers.name}`, {
+    const { data: account_environment } = await this.app.api.get(`/accounts/${fetched_account.id}/environments/${answers.environment}`);
+    await this.app.api.put(`/environments/${account_environment.id}`, {
       host: answers.host,
       service_token: await readIfFile(answers.service_token),
       cluster_ca_certificate: await readIfFile(answers.cluster_ca_certificate),
