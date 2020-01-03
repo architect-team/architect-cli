3066b01252ae4fdadc63380a8450e6018c224ae3
TJ Higgins <tj@architect.io>
2020-01-03 14:55:55 -0500

    Fix invalid table for environments

--- src/commands/environments/index.ts
+++ src/commands/environments/index.ts
@@ -19,11 +19,11 @@ export default class Environments extends Command {
 
     const { rows: environments } = (await this.app.api.get(`/environments?q=${args.query || ''}`)).data;
 
-    const table = new Table({ head: ['Name', 'Account', 'Created', 'Updated'] });
+    const table = new Table({ head: ['Name', 'Account', 'Namespace', 'Created', 'Updated'] });
     for (const env of environments) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
       // @ts-ignore
-      table.push([env.name, env.namespace, env.created_at, env.updated_at]);
+      table.push([env.name, env.account.name, env.namespace, env.created_at, env.updated_at]);
     }
 
     this.log(table.toString());
