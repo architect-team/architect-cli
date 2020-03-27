54d1b3a6f9d020f74510322189c9331c25b79496
dan-barrett <daniel.barrett@architect.io>
2020-03-27 14:57:58 -0400

    add account-not-found message and sanitize image tags

--- src/common/utils/docker.ts
+++ src/common/utils/docker.ts
@@ -74,3 +74,28 @@ export const parseImageLabel = async (image_ref: string, label_name: string) => 
   const { stdout } = await docker(['inspect', image_ref, '--format', `{{ index .Config.Labels "${label_name}"}}`], { stdout: false });
   return JSON.parse(stdout);
 };
+
+/**
+ * this method splits the tag off of an image string. this logic is not straightforward as the image string may contain a port.
+ *
+ * The key differentiator (and the only way it's logically possible) is that a tag cannot contain a slash. So we look for the last ":" with no trailing "/"
+ * @param image
+ */
+export const strip_tag_from_image: (i: string) => string = (image: string) => {
+  if (image.includes('://')) { // remove protocol if exists
+    image = image.split('://')[1];
+  }
+
+  const split = image.split(':');
+  if (split.length === 1) { // no colon indicates a dockerhub native, tagless image (ie postgres)
+    return split[0];
+  } else if (split.length === 2 && split[1].includes('/')) { // one colon with a slash afterwards indicates a port with no tag (ie privaterepo.com:443/postgres)
+    return image;
+  } else if (split.length === 2 && !split[1].includes('/')) { // one colon with NO slash afterwards indicates a tag (ie privaterepo.com/postgres:10 or postgres:10)
+    return split[0];
+  } else if (split.length === 3) { // two colons indicates a port and a tag (ie privaterepo.com:443/postgres:10)
+    return `${split[0]}:${split[1]}`;
+  } else {
+    throw new Error(`Invalid docker image format: ${image}`);
+  }
+};
