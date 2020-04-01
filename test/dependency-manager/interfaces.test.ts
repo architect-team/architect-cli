78f86ef42e14d62b9d69dadcb910fc8ad158b079
Ryan Cahill <ryan.cahill444@gmail.com>
2020-04-01 16:30:49 -0400

    adding test for interfaces from env config

---
+++ test/dependency-manager/interfaces.test.ts
@@ -0,0 +1,177 @@
+import { expect } from '@oclif/test';
+import axios from 'axios';
+import mock_fs from 'mock-fs';
+import sinon from 'sinon';
+import Build from '../../src/commands/build';
+import LocalDependencyGraph from '../../src/common/dependency-manager/local-graph';
+import LocalDependencyManager from '../../src/common/dependency-manager/local-manager';
+import { LocalServiceNode } from '../../src/common/dependency-manager/local-service-node';
+import { ExternalNode } from '../../src/dependency-manager/src/graph/node/external';
+import { ValueFromParameter } from '../../src/dependency-manager/src/manager';
+
+describe('manager parameters', function () {
+
+  beforeEach(async () => {
+    // Stub the logger
+    sinon.replace(Build.prototype, 'log', sinon.stub());
+
+    const backend_json = {
+      "name": "architect/backend",
+      "interfaces": {
+        "main": {
+          "description": "main port",
+          "port": "8080"
+        },
+        "secondary": {
+          "description": "secondary port",
+          "port": "8081"
+        }
+      }
+    };
+
+    const frontend_main_json = {
+      "name": "architect/frontend-main",
+      "dependencies": {
+        "architect/backend": "latest"
+      },
+      "parameters": {
+        "API_ADDR": {
+          "default": {
+            "valueFrom": {
+              "dependency": "architect/backend:latest",
+              "interface": "main",
+              "value": "$MAIN_HOST:$MAIN_PORT"
+            }
+          }
+        }
+      }
+    };
+
+    const frontend_secondary_json = {
+      "name": "architect/frontend-secondary",
+      "dependencies": {
+        "architect/backend": "latest"
+      },
+      "parameters": {
+        "API_ADDR": {
+          "default": {
+            "valueFrom": {
+              "dependency": "architect/backend:latest",
+              "interface": "secondary",
+              "value": "$SECONDARY_HOST:$SECONDARY_PORT"
+            }
+          }
+        }
+      }
+    };
+
+    const env_config_internal = {
+      "services": {
+        "architect/frontend-main:latest": {
+          "debug": {
+            "path": "./src/frontend-main",
+          }
+        },
+        "architect/frontend-secondary:latest": {
+          "debug": {
+            "path": "./src/frontend-secondary",
+          }
+        },
+        "architect/backend:latest": {
+          "debug": {
+            "path": "./src/backend",
+          }
+        }
+      }
+    };
+
+    const env_config_external = {
+      "services": {
+        "architect/frontend-main:latest": {
+          "debug": {
+            "path": "./src/frontend-main",
+          }
+        },
+        "architect/frontend-secondary:latest": {
+          "debug": {
+            "path": "./src/frontend-secondary",
+          }
+        },
+        "architect/backend:latest": {
+          "debug": {
+            "path": "./src/backend",
+          },
+          "interfaces": {
+            "main": {
+              "description": "main port",
+              "host": "main.host",
+              "port": "8080"
+            },
+            "secondary": {
+              "description": "secondary port",
+              "host": "secondary.host",
+              "port": "8081"
+            }
+          }
+        }
+      }
+    };
+
+    mock_fs({
+      '/stack/src/frontend-main/architect.json': JSON.stringify(frontend_main_json),
+      '/stack/src/frontend-secondary/architect.json': JSON.stringify(frontend_secondary_json),
+      '/stack/src/backend/architect.json': JSON.stringify(backend_json),
+      '/stack/arc.env.internal.json': JSON.stringify(env_config_internal),
+      '/stack/arc.env.external.json': JSON.stringify(env_config_external),
+    });
+  });
+
+  afterEach(function () {
+    // Restore stubs
+    sinon.restore();
+    // Restore fs
+    mock_fs.restore();
+  });
+
+  it('valueFrom port from service interfaces', async () => {
+    const manager = await LocalDependencyManager.createFromPath(axios.create(), '/stack/arc.env.internal.json')
+    const graph: LocalDependencyGraph = manager.graph;
+
+    const backend_node = graph.nodes.find(node => node.ref === 'architect/backend:latest') as LocalServiceNode;
+    expect(backend_node!.parameters.MAIN_PORT).eq('8080');
+    expect(backend_node!.parameters.SECONDARY_PORT).eq('8081');
+    expect(backend_node!.ports.filter(port_pair => port_pair.target.toString() === '8080').length).eq(1);
+    expect(backend_node!.ports.filter(port_pair => port_pair.target.toString() === '8081').length).eq(1);
+    expect(backend_node!.service_config.getInterfaces().main.port).eq('8080');
+    expect(backend_node!.service_config.getInterfaces().secondary.port).eq('8081');
+
+    const frontend_main_node = graph.nodes.find(node => node.ref === 'architect/frontend-main:latest') as LocalServiceNode;
+    const interfaced_main_value_from = frontend_main_node!.service_config.getParameters().API_ADDR.default as ValueFromParameter;
+    expect(interfaced_main_value_from.valueFrom.interface).eq('main');
+    expect(frontend_main_node!.parameters.API_ADDR).eq(`${backend_node.normalized_ref}:8080`);
+
+    const frontend_secondary_node = graph.nodes.find(node => node.ref === 'architect/frontend-secondary:latest') as LocalServiceNode;
+    const interfaced_secondary_value_from = frontend_secondary_node!.service_config.getParameters().API_ADDR.default as ValueFromParameter;
+    expect(interfaced_secondary_value_from.valueFrom.interface).eq('secondary');
+    expect(frontend_secondary_node!.parameters.API_ADDR).eq(`${backend_node.normalized_ref}:8081`);
+  });
+
+  it('valueFrom port from environment interfaces', async () => {
+    const manager = await LocalDependencyManager.createFromPath(axios.create(), '/stack/arc.env.external.json')
+    const graph: LocalDependencyGraph = manager.graph;
+
+    const backend_node = graph.nodes.find(node => node.ref === 'architect/backend:latest') as ExternalNode;
+    expect(backend_node.interfaces!.main.port).eq('8080');
+    expect(backend_node.interfaces!.secondary.port).eq('8081');
+
+    const frontend_main_node = graph.nodes.find(node => node.ref === 'architect/frontend-main:latest') as LocalServiceNode;
+    const interfaced_main_value_from = frontend_main_node!.service_config.getParameters().API_ADDR.default as ValueFromParameter;
+    expect(interfaced_main_value_from.valueFrom.interface).eq('main');
+    expect(frontend_main_node!.parameters.API_ADDR).eq(`main.host:8080`);
+
+    const frontend_secondary_node = graph.nodes.find(node => node.ref === 'architect/frontend-secondary:latest') as LocalServiceNode;
+    const interfaced_secondary_value_from = frontend_secondary_node!.service_config.getParameters().API_ADDR.default as ValueFromParameter;
+    expect(interfaced_secondary_value_from.valueFrom.interface).eq('secondary');
+    expect(frontend_secondary_node!.parameters.API_ADDR).eq(`secondary.host:8081`);
+  });
+});
--- test/dependency-manager/parameters.test.ts
+++ test/dependency-manager/parameters.test.ts
@@ -6,8 +6,6 @@ import sinon from 'sinon';
 import Build from '../../src/commands/build';
 import LocalDependencyGraph from '../../src/common/dependency-manager/local-graph';
 import LocalDependencyManager from '../../src/common/dependency-manager/local-manager';
-import { LocalServiceNode } from '../../src/common/dependency-manager/local-service-node';
-import { ValueFromParameter } from '../../src/dependency-manager/src/manager';
 
 describe('manager parameters', function () {
   let graph: LocalDependencyGraph;
@@ -48,77 +46,5 @@ describe('manager parameters', function () {
   it('value override valueFrom', async () => {
     const addition_node = graph.nodes.find((node) => node.ref === 'architect/addition-service-rest:latest')!;
     expect(addition_node.parameters.DB_PRIMARY_PORT).eq('5432');
-  });
-
-
-  it('valueFrom port from interface', async () => {
-    const backend_json = {
-      "name": "architect/backend",
-      "interfaces": {
-        "main": {
-          "description": "main port",
-          "port": "8080"
-        },
-        "secondary": {
-          "description": "secondary port",
-          "port": "8081"
-        }
-      }
-    };
-
-    const frontend_json = {
-      "name": "architect/frontend",
-      "dependencies": {
-        "architect/backend": "latest"
-      },
-      "parameters": {
-        "API_ADDR": {
-          "default": {
-            "valueFrom": {
-              "dependency": "architect/backend:latest",
-              "interface": "main",
-              "value": "$MAIN_HOST:$MAIN_PORT"
-            }
-          }
-        }
-      }
-    }
-
-    const env_config = {
-      "services": {
-        "architect/frontend:latest": {
-          "debug": {
-            "path": "./src/frontend",
-          }
-        },
-        "architect/backend:latest": {
-          "debug": {
-            "path": "./src/backend",
-          }
-        }
-      }
-    }
-
-    mock_fs({
-      '/stack/src/frontend/architect.json': JSON.stringify(frontend_json),
-      '/stack/src/backend/architect.json': JSON.stringify(backend_json),
-      '/stack/arc.env.json': JSON.stringify(env_config),
-    });
-
-    const manager = await LocalDependencyManager.createFromPath(axios.create(), '/stack/arc.env.json');
-    graph = manager.graph;
-
-    const backend_node = graph.nodes.find(node => node.ref === 'architect/backend:latest') as LocalServiceNode;
-    expect(backend_node!.parameters.MAIN_PORT).eq('8080');
-    expect(backend_node!.parameters.SECONDARY_PORT).eq('8081');
-    expect(backend_node!.ports.filter(port_pair => port_pair.target.toString() === '8080').length).eq(1);
-    expect(backend_node!.ports.filter(port_pair => port_pair.target.toString() === '8081').length).eq(1);
-    expect(backend_node!.service_config.getInterfaces().main.port).eq('8080');
-    expect(backend_node!.service_config.getInterfaces().secondary.port).eq('8081');
-
-    const frontend_node = graph.nodes.find(node => node.ref === 'architect/frontend:latest') as LocalServiceNode;
-    const interfaced_value_from = frontend_node!.service_config.getParameters().API_ADDR.default as ValueFromParameter;
-    expect(interfaced_value_from.valueFrom.interface).eq('main');
-    expect(frontend_node!.parameters.API_ADDR).eq('architect.backend.latest:8080');
   });
 });
