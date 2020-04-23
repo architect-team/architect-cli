beb0599d041f8bc7b2cbef9e428d37ed3069fd8f
Ryan Cahill <ryan.cahill444@gmail.com>
2020-04-23 10:58:13 -0400

    kicking off tests again

--- test/commands/logout.test.ts
+++ test/commands/logout.test.ts
@@ -34,7 +34,6 @@ describe('logout', () => {
     sinon.replace(CredentialManager.prototype, 'delete', credential_spy);
 
     await Logout.run();
-
     expect(credential_spy.getCalls().length).to.equal(2);
     expect(credential_spy.firstCall.args[0]).to.equal('architect.io');
     expect(credential_spy.secondCall.args[0]).to.equal('architect.io/token');
