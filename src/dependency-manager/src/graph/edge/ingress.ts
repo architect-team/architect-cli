f2ca8cd10ad428e93bf3d498dc57a9272177f121
Ryan Cahill <ryan.cahill444@gmail.com>
2020-05-18 11:52:48 -0400

    updating edge interfaces name

--- src/dependency-manager/src/graph/edge/ingress.ts
+++ src/dependency-manager/src/graph/edge/ingress.ts
@@ -3,10 +3,10 @@ import { ServiceInterfaceSpec } from '../../service-config/base';
 
 export default class IngressEdge extends DependencyEdge {
   __type = 'ingress';
-  interfaces: { [s: string]: ServiceInterfaceSpec };
+  to_interfaces: { [s: string]: ServiceInterfaceSpec };
 
   constructor(from: string, to: string, interfaces: { [s: string]: ServiceInterfaceSpec }) {
     super(from, to);
-    this.interfaces = interfaces;
+    this.to_interfaces = interfaces;
   }
 }
