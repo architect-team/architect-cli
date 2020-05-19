7d6112a67435be4062eb9987c9e3c5e9ca90c3bb
Ryan Cahill <ryan.cahill444@gmail.com>
2020-05-19 11:37:45 -0400

    removing interfaces from ingress edges

--- src/common/dependency-manager/local-manager.ts
+++ src/common/dependency-manager/local-manager.ts
@@ -2,7 +2,7 @@ import { AxiosInstance } from 'axios';
 import chalk from 'chalk';
 import fs from 'fs-extra';
 import path from 'path';
-import DependencyManager, { DependencyNode, EnvironmentConfigBuilder, ServiceConfig, ServiceConfigBuilder, ServiceInterfaceSpec, ServiceNode } from '../../dependency-manager/src';
+import DependencyManager, { DependencyNode, EnvironmentConfigBuilder, ServiceConfig, ServiceConfigBuilder, ServiceNode } from '../../dependency-manager/src';
 import IngressEdge from '../../dependency-manager/src/graph/edge/ingress';
 import GatewayNode from '../../dependency-manager/src/graph/node/gateway';
 import { Dictionary } from '../../dependency-manager/src/utils/dictionary';
@@ -36,18 +36,13 @@ export default class LocalDependencyManager extends DependencyManager {
       const svc_node = await dependency_manager.loadServiceFromConfig(config);
       if (svc_node instanceof ServiceNode) {
         const interfaces = svc_node.node_config.getInterfaces();
-        const external_interfaces: { [s: string]: ServiceInterfaceSpec } = {};
-        for (const [name, details] of Object.entries(interfaces)) {
-          if (details.subdomain) {
-            external_interfaces[name] = details;
-          }
-        }
+        const external_interfaces_count = Object.values(interfaces).filter(i => i.subdomain).length;
         const interface_count = Object.keys(interfaces).length;
-        if (interface_count === 1 && Object.keys(external_interfaces).length) { // max one interface per container if external exists https://github.com/nginx-proxy/nginx-proxy#multiple-ports
+        if (interface_count === 1 && external_interfaces_count) { // max one interface per container if external exists https://github.com/nginx-proxy/nginx-proxy#multiple-ports
           const gateway = new GatewayNode();
           dependency_manager.graph.addNode(gateway);
-          dependency_manager.graph.addEdge(new IngressEdge(gateway.ref, svc_node.ref, external_interfaces));
-        } else if (interface_count > 1 && external_interfaces.length > 1) {
+          dependency_manager.graph.addEdge(new IngressEdge(gateway.ref, svc_node.ref));
+        } else if (interface_count > 1 && external_interfaces_count > 1) {
           throw new Error(`Error in service definition for ${svc_node.ref}. Only one ingress per service is supported locally.`);
         }
       }
--- src/dependency-manager/src/graph/edge/ingress.ts
+++ src/dependency-manager/src/graph/edge/ingress.ts
@@ -1,12 +1,9 @@
 import DependencyEdge from '.';
-import { ServiceInterfaceSpec } from '../../service-config/base';
 
 export default class IngressEdge extends DependencyEdge {
   __type = 'ingress';
-  to_interfaces: { [s: string]: ServiceInterfaceSpec };
 
-  constructor(from: string, to: string, to_interfaces: { [s: string]: ServiceInterfaceSpec }) {
+  constructor(from: string, to: string) {
     super(from, to);
-    this.to_interfaces = to_interfaces;
   }
 }
