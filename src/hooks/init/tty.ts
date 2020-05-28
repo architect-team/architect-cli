79d8a92f7393613767bed8e975bd90491ed1ba09
TJ Higgins <tj@architect.io>
2020-05-28 18:54:54 -0400

    Add hack to support windows with Inquirer >=7.1.0

--- src/hooks/init/tty.ts
+++ src/hooks/init/tty.ts
@@ -1,7 +1,13 @@
 import { Hook } from '@oclif/config';
+import readline from 'readline';
 import PromptUtils from '../../common/utils/prompt_utils';
 
 const hook: Hook<'init'> = async function (options) {
+  // https://github.com/SBoudrias/Inquirer.js#know-issues
+  if (/^win/i.test(process.platform)) {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    readline.Interface.prototype.close = () => { };
+  }
   if (!PromptUtils.prompts_available()) {
     PromptUtils.disable_prompts();
   }
