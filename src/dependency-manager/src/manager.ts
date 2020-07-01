0e216563627086ae533d8e842b94c6fbacd856f0
TJ Higgins <tj@architect.io>
2020-07-01 12:29:05 -0400

    Add required children prop to validation errors

--- src/dependency-manager/src/manager.ts
+++ src/dependency-manager/src/manager.ts
@@ -241,6 +241,7 @@ export default abstract class DependencyManager {
           validation_error.constraints = {
             'Required': `${pk} is required`,
           };
+          validation_error.children = [];
           validation_errors.push(validation_error);
         }
       }
