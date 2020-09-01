015772e1a2c2d193664d63c17b809e255c6c65f7
Ryan Cahill <ryan.cahill444@gmail.com>
2020-09-01 15:22:31 -0400

    updating tests

--- test/commands/login.test.ts
+++ test/commands/login.test.ts
@@ -9,7 +9,7 @@ describe('login', () => {
   test
     .timeout(10000)
     .stderr({ print })
-    .command(['login', '-u', 'test-user'])
+    .command(['login', '-e', 'test-user'])
     .catch(ctx => {
       expect(ctx.message).to.contain('password is required')
     })
@@ -19,7 +19,7 @@ describe('login', () => {
     .stderr({ print })
     .command(['login'])
     .catch(ctx => {
-      expect(ctx.message).to.contain('We detected that this environment does not have a prompt available. To login in a non-tty environment, please use both the user and password options: `architect login -u <user> -p <password>`')
+      expect(ctx.message).to.contain('We detected that this environment does not have a prompt available. To login in a non-tty environment, please use both the user and password options: `architect login -e <user> -p <password>`')
     })
     .it('browser login flow throws when not in a tty environment');
 
