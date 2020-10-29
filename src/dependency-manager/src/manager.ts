e8846fdc0781a1bdcf619747804fc9444ad095d0
Ryan Cahill <ryan.cahill444@gmail.com>
2020-10-29 10:50:10 -0400

    using slug utils

--- src/dependency-manager/src/manager.ts
+++ src/dependency-manager/src/manager.ts
@@ -132,8 +132,8 @@ export default abstract class DependencyManager {
       const dep_component = ComponentConfigBuilder.buildFromJSON({ extends: dep_extends, name: dep_name });
 
       if (component_map[dep_component.getRef()]) {
-        const first_side_dependency = component.getRef().split(':')[0] in component_map[dep_component.getRef()].getDependencies();
-        const second_side_dependency = dep_component.getRef().split(':')[0] in component.getDependencies();
+        const first_side_dependency = ComponentVersionSlugUtils.parse(component.getRef()).namespaced_component_name in component_map[dep_component.getRef()].getDependencies();
+        const second_side_dependency = ComponentVersionSlugUtils.parse(dep_component.getRef()).namespaced_component_name in component.getDependencies();
         if (first_side_dependency && second_side_dependency) {
           throw new Error(`Circular component dependency detected (${ component.getRef() } <> ${dep_component.getRef()})`);
         }
