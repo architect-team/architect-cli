f3b6e502fbd97f241b0cf2f5f3cc232f63770b8b
Ryan Cahill <ryan.cahill444@gmail.com>
2020-12-28 10:19:15 -0500

    resetting test flag

--- test/commands/deploy.test.ts
+++ test/commands/deploy.test.ts
@@ -9,7 +9,7 @@ import { ComponentConfigBuilder, EnvironmentConfigBuilder } from '../../src/depe
 import { mockArchitectAuth, MOCK_API_HOST } from '../utils/mocks';
 
 // set to true while working on tests for easier debugging; otherwise oclif/test eats the stdout/stderr
-const print = true; // TODO: reset
+const print = false;
 
 const account = {
   id: 'test-account-id',
