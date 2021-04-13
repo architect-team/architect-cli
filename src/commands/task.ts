6b070cf2017ffffc3b3e6216a3901b93520dd5f6
Ryan Cahill <ryan.cahill444@gmail.com>
2021-04-12 22:00:48 -0400

    searching for the task among the running tasks properly

--- src/commands/task.ts
+++ src/commands/task.ts
@@ -82,7 +82,8 @@ export default class TaskExec extends Command {
     let service_name;
     const slug = ServiceVersionSlugUtils.build(parsed_slug.component_account_name, parsed_slug.component_name, args.task, parsed_slug.tag);
     const ref = Refs.safeRef(slug);
-    const matching_names = Object.keys(compose.services).filter(name => name.includes(ref));
+    const service_hash = ref.substring(ref.lastIndexOf('-') + 1);
+    const matching_names = Object.keys(compose.services).filter(name => name.endsWith(service_hash));
     if (!matching_names?.length) {
       throw new Error(`Could not find ${args.component}/${args.task} running in your local ${project_name} environment. See ${compose_file} for available tasks and services.`);
     } else if (matching_names.length > 1) {
