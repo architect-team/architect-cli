6904f422416e868cf8437a4dc5be6fc9cecfc008
Ryan Cahill <ryan.cahill444@gmail.com>
2021-04-13 10:50:32 -0400

    updated checks

--- src/commands/task.ts
+++ src/commands/task.ts
@@ -82,11 +82,9 @@ export default class TaskExec extends Command {
     let service_name;
     const slug = ServiceVersionSlugUtils.build(parsed_slug.component_account_name, parsed_slug.component_name, args.task, parsed_slug.tag);
     const ref = ComponentConfig.getNodeRef(slug);
-    const matching_names = Object.keys(compose.services).filter(name => name.includes(ref));
+    const matching_names = Object.keys(compose.services).find(name => name === ref);
     if (!matching_names?.length) {
       throw new Error(`Could not find ${args.component}/${args.task} running in your local ${project_name} environment. See ${compose_file} for available tasks and services.`);
-    } else if (matching_names.length > 1) {
-      throw new Error(`There was more than one task in your local environment that matched ${args.component}/${args.task} running in your local environment. See ${compose_file} for available tasks and services.`);
     } else {
       service_name = matching_names[0];
     }
