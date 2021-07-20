313b8ab21b0b6bb2b4129d8a5e34154ecca56d32
Ryan Cahill <ryan.cahill444@gmail.com>
2021-07-20 12:16:10 -0400

    adding semver dependency, using this.error

--- src/hooks/init/check-version.ts
+++ src/hooks/init/check-version.ts
@@ -4,8 +4,7 @@ import semver from 'semver';
 const hook: Hook<'init'> = async function (options) {
   const version = require('../../../package').engines.node;
   if (!semver.satisfies(process.version, version)) {
-    console.log(`The required node version ${version} for the Architect CLI is not satisfied by your current node version ${process.version}.`);
-    process.exit(1);
+    this.error(`The required node version ${version} for the Architect CLI is not satisfied by your current node version ${process.version}.`);
   }
 };
 
