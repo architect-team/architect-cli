03b27a4bf58e61425542c7cb1c4453ee1fafcb57
Ryan Cahill <ryan.cahill444@gmail.com>
2021-07-20 10:34:19 -0400

    importing package.json with require

--- src/hooks/init/check-version.ts
+++ src/hooks/init/check-version.ts
@@ -1,9 +1,8 @@
 import { Hook } from '@oclif/config';
 import semver from 'semver';
-import { engines } from '../../../package';
 
 const hook: Hook<'init'> = async function (options) {
-  const version = engines.node;
+  const version = require('../../../package').engines.node;
   if (!semver.satisfies(process.version, version)) {
     console.log(`The required node version ${version} for the Architect CLI is not satisfied by your current node version ${process.version}.`);
     process.exit(1);
