a73067d6947667a1542cce2f56f024c12859b30c
Dan <daniel.barrett@architect.io>
2021-08-12 16:01:02 -0400

    remove logging

--- src/dependency-manager/src/schema/spec/transform/resource-transform.ts
+++ src/dependency-manager/src/schema/spec/transform/resource-transform.ts
@@ -98,8 +98,6 @@ export const transformVolumeSpec = (key: string, volume: VolumeSpec | string): V
 
 export const transformResourceSpec = (key: string, spec: ResourceSpec, component_ref: string, tag: string): ResourceConfig => {
   const environment = transformResourceSpecEnvironment(spec.environment);
-  console.log('component_ref');
-  console.log(component_ref);
   const { component_account_name, component_name } = ComponentSlugUtils.parse(component_ref);
   return {
     name: key,
