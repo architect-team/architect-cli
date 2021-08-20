23d730936ba30d37feecfb9e58ae400475af9fe4
Dan <daniel.barrett@architect.io>
2021-08-20 10:32:24 -0400

    use instance_name in service/task ref

--- src/dependency-manager/src/schema/spec/transform/task-transform.ts
+++ src/dependency-manager/src/schema/spec/transform/task-transform.ts
@@ -1,13 +1,14 @@
+import { ComponentInstanceMetadata } from '../../config/component-config';
 import { TaskConfig } from '../../config/task-config';
 import { TaskSpec } from '../task-spec';
 import { transformResourceSpec } from './resource-transform';
 
-export const transformTaskSpec = (key: string, spec: TaskSpec, component_ref: string, tag: string): TaskConfig => {
-  const resource_config = transformResourceSpec(key, spec, component_ref, tag);
+export const transformTaskSpec = (key: string, spec: TaskSpec, component_ref: string, tag: string, instance_metadata?: ComponentInstanceMetadata): TaskConfig => {
+  const resource_config = transformResourceSpec(key, spec, component_ref, tag, instance_metadata);
 
   return {
     ...resource_config,
-    debug: spec.debug ? transformTaskSpec(key, spec.debug, component_ref, tag) : undefined,
+    debug: spec.debug ? transformTaskSpec(key, spec.debug, component_ref, tag, instance_metadata) : undefined,
     schedule: spec.schedule,
   };
 };
