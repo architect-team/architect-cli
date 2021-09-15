98e1c789f0f041d50b3c43833e8b8b3d32b2fcf7
Ryan Cahill <ryan.cahill444@gmail.com>
2021-09-15 12:07:33 -0400

    adding component interface configs to interface nodes

--- src/dependency-manager/src/graph/node/interfaces.ts
+++ src/dependency-manager/src/graph/node/interfaces.ts
@@ -1,18 +1,26 @@
 import { DependencyNode } from '.';
+import { ComponentInterfaceConfig, ServiceInterfaceConfig } from '../..';
+import { Dictionary } from '../../utils/dictionary';
 
 export default class InterfacesNode extends DependencyNode {
   __type = 'interfaces';
 
   ref!: string;
   slug!: string;
+  config!: Dictionary<ComponentInterfaceConfig>;
 
-  constructor(ref: string, slug: string) {
+  constructor(ref: string, slug: string, config: Dictionary<ComponentInterfaceConfig>) {
     super();
     this.ref = ref;
     this.slug = slug;
+    this.config = config;
   }
 
-  get interfaces(): { [key: string]: any } {
+  get interfaces(): Dictionary<ServiceInterfaceConfig> {
     return {};
+  }
+
+  get component_interfaces(): Dictionary<ComponentInterfaceConfig> {
+    return this.config;
   }
 }
