e9e620a4d69de0e1799f5f48291a9dafb74a1b38
Michael Muesch <mueschm@gmail.com>
2022-04-06 11:37:00 -0400

    Added support for semantic release. (#546)
    * Added support for semantic release.
    
    * master is main and arc-* is for prerelease.
    
    * Swith over to release.config.js
    
    * Remove cross-env
    
    Co-authored-by: Michael Muesch <michael.muesch@architect.io>

---
+++ release.config.js
@@ -0,0 +1,68 @@
+const { GIT_BRANCH: branch } = process.env;
+
+const plugins = [
+  "@semantic-release/commit-analyzer",
+  "@semantic-release/release-notes-generator",
+  [
+    "@semantic-release/changelog",
+    {
+      "changelogFile": "CHANGELOG.md"
+    }
+  ],
+  [
+    "@semantic-release/git",
+    {
+      "assets": [
+        "CHANGELOG.md",
+        "README.md",
+        "package.json",
+        "package-lock.json"
+      ]
+    }
+  ],
+  [
+    "@semantic-release/github",
+    {
+      "assets": [
+        {
+          "path": "dist/*.tar.gz",
+          "label": "Architect-CLI ${nextRelease.version}"
+        }
+      ]
+    }
+  ],
+  [
+    "@semantic-release/exec",
+    {
+      "publishCmd": "npm run pack"
+    }
+  ],
+  "@semantic-release/npm"
+];
+
+const main_plugins = plugins.concat([[
+  "@semantic-release/github",
+  {
+    "assets": [
+      {
+        "path": "dist/*.tar.gz",
+        "label": "Architect-CLI ${nextRelease.version}"
+      }
+    ]
+  }
+]]);
+
+module.exports = {
+  "branches": [
+    "main",
+    {
+      "name": "rc",
+      "prerelease": true
+    },
+    {
+      "name": "arc-*",
+      "prerelease": true
+    }
+  ],
+  plugins: branch === 'main' ? main_plugins : plugins,
+};
