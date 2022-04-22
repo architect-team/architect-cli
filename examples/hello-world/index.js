f57c7e4ecf3bdb33145026844312599786501d44
Ryan Cahill <ryan.cahill444@gmail.com>
2022-04-22 10:49:57 -0400

    updating tests and other small issues

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express();
+const express = require('express');
+const app = express();
 
-app.set('port', (process.env.PORT || 3000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(app.get('port'), () => {
+  console.log(`Node app is running at localhost: ${app.get('port')}`);
+});
