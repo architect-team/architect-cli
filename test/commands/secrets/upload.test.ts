c387fd9f67cf9759239cc9195f2aefd214acd664
Michael Muesch <mueschm@gmail.com>
2022-07-06 16:10:58 -0400

    Fix tests. (#611)
    Co-authored-by: Michael Muesch <michael.muesch@architect.io>

--- test/commands/secrets/upload.test.ts
+++ test/commands/secrets/upload.test.ts
@@ -1,8 +1,8 @@
+import { expect } from '@oclif/test';
 import sinon from 'sinon';
-import { expect, test } from '@oclif/test';
-import { MOCK_API_HOST } from '../../utils/mocks';
-import UserUtils from '../../../src/architect/user/user.utils';
 import SecretUtils from '../../../src/architect/secret/secret.utils';
+import UserUtils from '../../../src/architect/user/user.utils';
+import { mockArchitectAuth, MOCK_API_HOST } from '../../utils/mocks';
 
 describe('secrets', function () {
   // set to true while working on tests for easier debugging; otherwise oclif/test eats the stdout/stderr
@@ -22,10 +22,10 @@ describe('secrets', function () {
     ],
   }
 
-  const defaults = test
+  const defaults = mockArchitectAuth
     .nock(MOCK_API_HOST, api => api
       .get(`/accounts/${account.name}`)
-      .reply(200, account))
+      .reply(200, account));
 
   defaults
     .stub(UserUtils, 'isAdmin', async () => true)
@@ -39,7 +39,7 @@ describe('secrets', function () {
       expect(batch_update.callCount).to.eq(1);
       expect(ctx.stdout).to.contain('Successfully uploaded secrets');
     })
-  
+
   defaults
     .stub(UserUtils, 'isAdmin', async () => true)
     .stub(SecretUtils, 'getSecrets', sinon.stub())
@@ -52,7 +52,7 @@ describe('secrets', function () {
       expect(batch_update.callCount).to.eq(1);
       expect(ctx.stdout).to.contain('Successfully uploaded secrets');
     })
-  
+
   defaults
     .stub(UserUtils, 'isAdmin', async () => true)
     .stub(SecretUtils, 'getSecrets', sinon.stub())
@@ -65,7 +65,7 @@ describe('secrets', function () {
       expect(batch_update.callCount).to.eq(1);
       expect(ctx.stdout).to.contain('Successfully uploaded secrets');
     })
-  
+
   defaults
     .stub(UserUtils, 'isAdmin', async () => true)
     .stub(SecretUtils, 'getSecrets', sinon.stub())
@@ -78,11 +78,9 @@ describe('secrets', function () {
       expect(batch_update.callCount).to.eq(1);
       expect(ctx.stdout).to.contain('Successfully uploaded secrets');
     })
-  
+
   defaults
-    .nock(MOCK_API_HOST, api => api
-      .get(`/users/me`)
-      .reply(200, member_user))
+    .stub(UserUtils, 'isAdmin', async () => false)
     .stdout({ print })
     .stderr({ print })
     .command(['secrets:set', '-a', 'examples', './test/mocks/secrets/account-secrets.yml'])
