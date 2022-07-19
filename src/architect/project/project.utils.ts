16b2aa379ee9499e6c8250e93c10a6589985e45e
Michael Muesch <michael.muesch@architect.io>
2022-07-18 20:58:43 -0400

    Fix some small bugs

--- src/architect/project/project.utils.ts
+++ src/architect/project/project.utils.ts
@@ -1,13 +1,13 @@
 import fs from 'fs-extra';
+import inquirer from 'inquirer';
 import yaml from 'js-yaml';
+import fetch from 'node-fetch';
 import path from 'path';
 import untildify from 'untildify';
-import fetch from 'node-fetch';
-import inquirer from 'inquirer';
-import { Dictionary } from '../../dependency-manager/utils/dictionary';
 import { ComponentSpec } from '../../';
-import { EnvironmentSpecValue } from '../../dependency-manager/spec/resource-spec';
 import AppService from '../../app-config/service';
+import { EnvironmentSpecValue } from '../../dependency-manager/spec/resource-spec';
+import { Dictionary } from '../../dependency-manager/utils/dictionary';
 
 const download = require('download-git-repo');
 
@@ -21,7 +21,6 @@ interface Selection {
 const GITHUB_BRANCH = 'main';
 
 export default class ProjectUtils {
-  static readme: string[] = [];
 
   static async prompt(choices: any[], message: string): Promise<any> {
     inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
@@ -93,7 +92,7 @@ export default class ProjectUtils {
     if (!backend_yml.services || !database_yml.services) {
       return;
     }
-    
+
     for (const [service_key, service] of Object.entries(backend_yml.services)) {
       if (service.image) {
         backend_yml.secrets = { ...backend_yml.secrets, ...database_yml.secrets };
@@ -103,12 +102,12 @@ export default class ProjectUtils {
     }
   }
 
-  static async updateDependencies(frontend_yml: ComponentSpec, frontend_yml_path: string, backend_component_name: string): Promise<void> {
+  static async updateDependencies(frontend_yml: ComponentSpec, frontend_yml_path: string, backend_component_name?: string): Promise<void> {
     if (!frontend_yml.dependencies) {
       return;
     }
 
-    if (frontend_yml.dependencies && frontend_yml.services) {
+    if (frontend_yml.dependencies && frontend_yml.services && backend_component_name) {
       const new_dependencies: Dictionary<string> = {};
       for (const dep_val of Object.values(frontend_yml.dependencies)) {
         new_dependencies[backend_component_name] = dep_val;
@@ -125,36 +124,59 @@ export default class ProjectUtils {
     fs.writeFileSync(path.resolve(untildify(frontend_yml_path)), yaml.dump(frontend_yml));
   }
 
+  private static generateSingleComponentReadme(name: string): string {
+    return `# This project was auto-generated using the Architect CLI.
+### To run your project locally:
+Go into '${name}' and run the dev command.
+  % architect dev .
+### To deploy your project to the Cloud:
+Go into '${name}', then run the register and deploy commands
+  % architect register .
+  % architect deploy ${name}`;
+  }
+
+  private static generateMultiCoponentReadme(root_service: string, secondary_service: string) {
+    return `# This project was auto-generated using the Architect CLI.
+### To run your project locally:
+Go into '${secondary_service}', then run the link and link commands.
+  % architect link .
+Change directory to '../${root_service}' and run the dev command.
+  % architect dev .
+### To deploy your project to the Cloud:
+Go into '${secondary_service}' and run the register command.
+  % architect register .
+Change directory to '../${root_service}', then run the register and deploy commands
+  % architect register .
+  % architect deploy ${root_service}`;
+  }
+
   static async updateArchitectYamls(app: AppService, selections: Dictionary<any>, project_dir: string): Promise<void> {
     const backend = selections['backend'];
-    const backend_yml_path = `./${project_dir}/${backend.name.toLowerCase()}/architect.yml`;
-    const backend_yml = yaml.load(fs.readFileSync(backend_yml_path).toString('utf-8')) as ComponentSpec;
-    const database_yml = await this.fetchYamlFromGitHub(selections['database']['architect-file']);
-    this.switchDatabase(database_yml, backend_yml, backend_yml_path);
+    if (backend) {
+      const backend_yml_path = `./${project_dir}/${backend.name.toLowerCase()}/architect.yml`;
+      const backend_yml = yaml.load(fs.readFileSync(backend_yml_path).toString('utf-8')) as ComponentSpec;
+      const database_yml = await this.fetchYamlFromGitHub(selections['database']['architect-file']);
+      this.switchDatabase(database_yml, backend_yml, backend_yml_path);
+    }
 
+    // Need better handling for when a frontend requires a backend.
     const frontend = selections['frontend'];
-    const frontend_yml_path = `./${project_dir}/${frontend.name.toLowerCase()}/architect.yml`;
-    const frontend_yml = yaml.load(fs.readFileSync(frontend_yml_path).toString('utf-8')) as ComponentSpec;
-    const backend_component_name = backend.repository.split('/').pop();
-    await this.updateDependencies(frontend_yml, frontend_yml_path, backend_component_name);
+    if (frontend) {
+      const frontend_yml_path = `./${project_dir}/${frontend.name.toLowerCase()}/architect.yml`;
+      const frontend_yml = yaml.load(fs.readFileSync(frontend_yml_path).toString('utf-8')) as ComponentSpec;
+      const backend_component_name = backend ? backend.repository.split('/').pop() : undefined;
+      await this.updateDependencies(frontend_yml, frontend_yml_path, backend_component_name);
+    }
 
-    // create readme file
-    this.readme.push('# This project was auto-generated using the Architect CLI.\n');
-
-    this.readme.push(`### To run your project locally:`);
-    this.readme.push(`Go into '${backend.name.toLowerCase()}' and run the link command.`);
-    this.readme.push('  % architect link .\n');
-    this.readme.push(`Change directory to '../${frontend.name.toLowerCase()}', then run the link and dev commands.`);
-    this.readme.push('  % architect link .');
-    this.readme.push('  % architect dev .\n');
-
-    this.readme.push(`### To deploy your project to the Cloud:`);
-    this.readme.push(`Go into '${backend.name.toLowerCase()}' and run the register command.`);
-    this.readme.push('  % architect register .\n');
-    this.readme.push(`Change directory to '../${frontend.name.toLowerCase()}', then run the register and deploy commands`);
-    this.readme.push('  % architect register .');
-    this.readme.push(`  % architect deploy ${frontend.name.toLowerCase()}\n`);
-    fs.writeFileSync(`./${project_dir}/README.md`, this.readme.join('\n'));
+    let readme: string;
+    if (frontend && !backend) {
+      readme = this.generateSingleComponentReadme(frontend.name.toLowerCase());
+    } else if (!frontend && backend) {
+      readme = this.generateSingleComponentReadme(backend.name.toLowerCase());
+    } else {
+      readme = this.generateMultiCoponentReadme(frontend.name.toLowerCase(), backend.name.toLowerCase());
+    }
+    fs.writeFileSync(`./${project_dir}/README.md`, readme);
   }
 
   static async getSelections(chosen_project?: string): Promise<Dictionary<Selection>> {
@@ -162,9 +184,9 @@ export default class ProjectUtils {
     const config_file = 'https://raw.githubusercontent.com/architect-team/template-configs/main/config.json';
     const config_json = await this.fetchJsonFromGitHub(config_file) as Dictionary<any>;
     const choices = config_json.choices;
-    
+
     const selections: Dictionary<Selection> = {};
-    const types = ['Frontend', 'Backend', 'Full stack'];
+    const types = ['Full stack', 'Frontend', 'Backend'];
     const type = await this.prompt(types, 'Select a type you would like to proceed');
     if (type.toLowerCase() === 'frontend' || type.toLowerCase() === 'full stack') {
       const frontend_opts = choices.filter((item: any) => item.type === 'frontend');
@@ -172,13 +194,15 @@ export default class ProjectUtils {
       selections['frontend'] = frontend;
     }
 
-    const backend_opts = choices.filter((item: any) => item.type === 'backend');
-    const backend = await this.prompt(backend_opts, 'Select a backend');
-    selections['backend'] = backend;
+    if (type.toLowerCase() !== 'frontend') {
+      const backend_opts = choices.filter((item: any) => item.type === 'backend');
+      const backend = await this.prompt(backend_opts, 'Select a backend');
+      selections['backend'] = backend;
 
-    const database_opts = choices.filter((item: any) => item.type === 'database');
-    const database = await this.prompt(database_opts, 'Select a database');
-    selections['database'] = database;
+      const database_opts = choices.filter((item: any) => item.type === 'database');
+      const database = await this.prompt(database_opts, 'Select a database');
+      selections['database'] = database;
+    }
 
     return selections;
   }
