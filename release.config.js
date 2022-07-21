cd98eaea6617fd2b7363570f53607dc8eac66cd3
TJ Higgins <tj@architect.io>
2022-07-21 15:01:07 -0400

    remove docs from cli

--- release.config.js
+++ release.config.js
@@ -11,7 +11,7 @@ const git = [
       "README.md",
       "package.json",
       "package-lock.json",
-      "docs/5-reference/2-architect-yml.md",
+      "architect-yml.md",
       "src/dependency-manager/schema/architect.schema.json",
     ]
   }
