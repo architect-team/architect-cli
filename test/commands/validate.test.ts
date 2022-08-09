36b67af0794f096451f7e4a9b2b8a034a8d8cf27
Devin Sag <devin.sag@architect.io>
2022-08-09 17:06:03 -0400

    update for test cases

--- test/commands/validate.test.ts
+++ test/commands/validate.test.ts
@@ -44,4 +44,52 @@ describe('architect validate component', function () {
       expect(err.message).to.contain(path.resolve('non-existent/directory/architect.yml'));
     })
     .it('correctly fails on a non-existent directory and prints an error message');
+
+  mockArchitectAuth
+    .stdout({ print })
+    .stderr({ print })
+    .command(['validate', 'test/mocks/invalidschema/architect.yml'])
+    .catch(err => {
+      expect(process.exitCode).eq(1);
+    })
+    .it('correctly fails on an invalidation error with exit code 1');
+
+  mockArchitectAuth
+    .stdout({ print })
+    .stderr({ print })
+    .command(['validate', 'test/mocks/invalidschema/architect.yml'])
+    .catch(err => {
+      expect(err.stack).undefined;
+    })
+    .it('correctly fails on an invalidation error with no stacktrace');
+
+  mockArchitectAuth
+    .stdout({ print })
+    .stderr({ print })
+    .command(['validate', 'test/mocks/invalidschema/architect.yml'])
+    .catch(err => {
+      expect(err.name).eq(`ValidationErrors\ncomponent: tests/invalid_schema\nfile: ${path.resolve(`test/mocks/invalidschema/architect.yml`)}:1:6`);
+    })
+    .it('correctly displays prettyValidationErrors error message to screen in place of a stacktrace', ctx => {
+      const expected = [
+        'ValidationErrors\n',
+        'component: tests/invalid_schema\n',
+        'file: /Users/devinsag/code/architect/architect-cli/test/mocks/invalidschema/architect.yml:1:6\n',
+        '›  1 | name: tests/invalid_schema\n',
+        '     |      ﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋ must contain only lower alphanumeric and single hyphens or underscores in the middle; max length 32; optionally can be prefixed with a valid Architect account and separated by a slash (e.g. architect/component-name).\n',
+        '   2 | \n',
+        '   3 | services:\n',
+        '›  4 |   invalid_schema:\n',
+        '     |   ﹋﹋﹋﹋﹋﹋﹋ must contain only lower alphanumeric and single hyphens or underscores in the middle; max length 32\n',
+        '   5 |     interfaces:\n',
+        '   6 |       main: 8080\n',
+        '   7 | \n',
+        '   8 | interfaces:\n',
+        '›  9 |   invalid_schema: ${{ services.invalid_schema.interfaces.main.url }}\n',
+        '     |   ﹋﹋﹋﹋﹋﹋﹋ must contain only lower alphanumeric and single hyphens or underscores in the middle; max length 32\n',
+        '  10 | \n',
+      ];
+      expect(ctx.stderr).to.equal(expected.join(''));
+      expect(ctx.stdout).to.equal('');
+    });
 });
