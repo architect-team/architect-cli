9bd2bd9f7a74eb6a9f2c1f7a1f1e5e20892aab03
Mary Goncalves <mary.goncalves@architect.io>
2022-08-10 14:04:18 -0400

    List all linked components

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import untildify from 'untildify';
 import { buildSpecFromPath } from '../';
 import BaseCommand from '../base-command';
+import BaseTable from '../base-table';
+
 declare const process: NodeJS.Process;
 
 export default class Link extends BaseCommand {
@@ -10,7 +12,7 @@ export default class Link extends BaseCommand {
     return false;
   }
 
-  static description = 'Link a local component to the host to be used to power local deployments.';
+  static description = 'Link a local component to the host to be used to power local deployments or list all linked components.';
 
   static flags = {
     ...BaseCommand.flags,
@@ -18,27 +20,39 @@ export default class Link extends BaseCommand {
 
   static args = [{
     non_sensitive: true,
-    name: 'componentPath',
-    char: 'p',
+    name: 'componentPathOrList',
+    description: 'Provide a component path or list all linked components using "architect link list"',
     default: '.',
   }];
 
   static non_sensitive = new Set([...Object.keys({ ...Link.flags }), ...Link.args.map(arg => arg.name)]);
 
+  listLinkedComponents(): void {
+    const table = new BaseTable({ head: ['Component', 'Path'] });
+    for (const entry of Object.entries(this.app.linkedComponents)) {
+      table.push(entry);
+    }
+    this.log(table.toString());
+  }
+
   async run(): Promise<void> {
     const { args } = await this.parse(Link);
 
-    const component_path = path.resolve(untildify(args.componentPath));
-    // Try to load the component from the path to ensure it exists and is valid
-    try {
-      const component_config = buildSpecFromPath(component_path);
-      this.app.linkComponentPath(component_config.name, component_path);
-      this.log(`Successfully linked ${chalk.green(component_config.name)} to local system at ${chalk.green(component_path)}.`);
-    } catch (err: any) {
-      if (err.name === 'missing_config_file') {
-        this.log(chalk.red(err.message));
-      } else {
-        throw err;
+    if (args.componentPathOrList === 'list') {
+      this.listLinkedComponents();
+    } else {
+      const component_path = path.resolve(untildify(args.componentPathOrList));
+      // Try to load the component from the path to ensure it exists and is valid
+      try {
+        const component_config = buildSpecFromPath(component_path);
+        this.app.linkComponentPath(component_config.name, component_path);
+        this.log(`Successfully linked ${chalk.green(component_config.name)} to local system at ${chalk.green(component_path)}.`);
+      } catch (err: any) {
+        if (err.name === 'missing_config_file') {
+          this.log(chalk.red(err.message));
+        } else {
+          throw err;
+        }
       }
     }
   }
