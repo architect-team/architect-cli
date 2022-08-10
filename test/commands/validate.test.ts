b8b0367b5cef487e2536e25f683c5f14b6f15d53
Devin Sag <devin.sag@architect.io>
2022-08-10 09:58:44 -0400

    update test case names to be descriptive

--- test/commands/validate.test.ts
+++ test/commands/validate.test.ts
@@ -51,7 +51,7 @@ describe('architect validate component', function () {
   mockArchitectAuth
     .stdout({ print })
     .stderr({ print })
-    .command(['validate', 'test/mocks/invalidschema/architect.yml'])
+    .command(['validate', 'test/mocks/validationerrors/architect.yml'])
     .catch(err => {
       expect(process.exitCode).eq(1);
     })
@@ -60,7 +60,7 @@ describe('architect validate component', function () {
   mockArchitectAuth
     .stdout({ print })
     .stderr({ print })
-    .command(['validate', 'test/mocks/invalidschema/architect.yml'])
+    .command(['validate', 'test/mocks/validationerrors/architect.yml'])
     .catch(err => {
       expect(err.stack).undefined;
     })
@@ -69,32 +69,15 @@ describe('architect validate component', function () {
   mockArchitectAuth
     .stdout({ print })
     .stderr({ print })
-    .command(['validate', 'test/mocks/invalidschema/architect.yml'])
+    .command(['validate', 'test/mocks/validationerrors/architect.yml'])
     .catch(err => {
-      const expected_validation_error_name = `ValidationErrorscomponent: tests/invalid_schemafile: ${path.resolve(`test/mocks/invalidschema/architect.yml`)}:1:`;
-      const validation_error_name = err.name.split('\n');
-      const trimmed_validation_error_name = validation_error_name.map(line => line.replace(cr, '').trim());
-      expect(trimmed_validation_error_name.join('')).contains(expected_validation_error_name);
+      expect(err.name).to.contain('ValidationErrors');
+      expect(err.name).to.contain('component: tests/validation_errors');
+      expect(err.name).to.contain(`file: ${path.resolve(`test/mocks/validationerrors/architect.yml`)}`);
     })
     .it('correctly displays prettyValidationErrors error message to screen in place of a stacktrace', ctx => {
-      const expected_validation_error_stdout = [
-        '›  1 | name: tests/invalid_schema',
-        '|      ﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋ must contain only lower alphanumeric and single hyphens or underscores in the middle; max length 32; optionally can be prefixed with a valid Architect account and separated by a slash (e.g. architect/component-name).',
-        '2 |',
-        '3 | services:',
-        '›  4 |   invalid_schema:',
-        '|   ﹋﹋﹋﹋﹋﹋﹋ must contain only lower alphanumeric and single hyphens or underscores in the middle; max length 32',
-        '5 |     interfaces:',
-        '6 |       main: 8080',
-        '7 |',
-        '8 | interfaces:',
-        '›  9 |   invalid_schema: ${{ services.invalid_schema.interfaces.main.url }}',
-        '|   ﹋﹋﹋﹋﹋﹋﹋ must contain only lower alphanumeric and single hyphens or underscores in the middle; max length 32',
-        '10 |',
-      ];
-      const stdout_error_lines = ctx.stderr.split('\n');
-      const trimmed_stdout_error_lines = stdout_error_lines.map(line => line.replace(cr, '').trim());
-      expect(trimmed_stdout_error_lines.join('')).to.contain(expected_validation_error_stdout.join(''));
+      expect(ctx.stderr).to.contain('›  1 | name: tests/validation_errors');
+      expect(ctx.stderr).to.contain('must contain only lower alphanumeric and single hyphens or underscores in the middle; max length 32; optionally can be prefixed with a valid Architect account and separated by a slash (e.g. architect/component-name).');
       expect(ctx.stdout).to.equal('');
     });
 });
