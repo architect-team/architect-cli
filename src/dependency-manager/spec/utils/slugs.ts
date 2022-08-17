f8eb41a04e4fb82a951a7da7e95cc8a5c4fa2d9c
Devin Sag <devin.sag@architect.io>
2022-08-17 18:58:07 -0400

    add more invalid characters, and interface test

--- src/dependency-manager/spec/utils/slugs.ts
+++ src/dependency-manager/spec/utils/slugs.ts
@@ -35,8 +35,8 @@ export class Slugs {
   public static ComponentSecretRegexBase = `[a-zA-Z0-9_-]+`;
   public static ComponentSecretValidator = new RegExp(`^${Slugs.ComponentSecretRegexBase}$`);
 
-  public static ComponentSubdomainRegexBase = '([A-Za-z0-9](?:[A-Za-z0-9\\-]{0,61}[A-Za-z0-9])|[\\*|\\@])?';
-  public static ComponentSubdomainDescription = 'must contain alphanumeric character ([a-z0-9A-Z]), could contain dashes (-), underscores (_), and alphanumerics between.';
+  public static ComponentSubdomainRegexBase = '([A-Za-z0-9](?:[A-Za-z0-9\\-]{0,61}[A-Za-z0-9])|[^\\W\\D\\s\\t\\n\\r\\/]+|[\\@\\*]?)';
+  public static ComponentSubdomainDescription = 'must contain alphanumeric characters ([a-z0-9A-Z]), could contain dashes (-) and alphanumerics between.';
   public static ComponentSubdomainValidator = new RegExp(`^${Slugs.ComponentSubdomainRegexBase}$`);
 }
 
