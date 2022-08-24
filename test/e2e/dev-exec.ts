14d74775189c48be33f89efee93456d4263607c1
Tyler Aldrich <tyler.aldrich@architect.io>
2022-08-24 11:38:39 -0400

    Set ssl=false, cannot download certs in gh action

--- test/e2e/dev-exec.ts
+++ test/e2e/dev-exec.ts
@@ -15,7 +15,7 @@ function architect(args: string[], opts?: Options<string>) {
 
 // TODO: write example yml that requires no dependencies to tmpfile
 function runDev(shell: string): execa.ExecaChildProcess<string> {
-  const dev_process = architect(['dev', 'examples/hello-world/architect.yml', '--no-browser'], 
+  const dev_process = architect(['dev', 'examples/hello-world/architect.yml', '--no-browser', '--ssl=false'], 
     { shell, stdio: 'inherit' });
   
   // dev_process.stdout?.on('data', (data) => {
