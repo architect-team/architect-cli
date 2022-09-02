a7dacacebbe8a47a6bf71c8a4804c775551c7b76
Ryan Cahill <ryan.cahill444@gmail.com>
2022-09-02 16:32:44 -0400

    updating scale command and related tests

--- src/commands/scale.ts
+++ src/commands/scale.ts
@@ -9,8 +9,6 @@ import BaseCommand from '../base-command';
 import { ComponentVersionSlugUtils, ResourceSlugUtils } from '../dependency-manager/spec/utils/slugs';
 import { ComponentVersion } from './components/versions';
 
-// export type ScalingSettings = Dictionary<Dictionary<{ replicas?: number }>>; // TODO: export and use for API as well?
-
 export default class Scale extends BaseCommand {
   static description = 'Scale a service to a specified number of replicas.';
 
@@ -66,10 +64,10 @@ export default class Scale extends BaseCommand {
       service_name = answers.service_name;
     }
 
-    const resource_slug = ResourceSlugUtils.build(account.name, component_name, 'services', service_name, instance_name);
+    // TODO: warning about alpha release
+    const resource_slug = ResourceSlugUtils.build(undefined, component_name, 'services', service_name, instance_name);
     const dto = {
       resource_slug,
-      // tag: component_tag, // TODO: remove the tag?
       replicas: flags.replicas,
     };
     try {
@@ -77,12 +75,8 @@ export default class Scale extends BaseCommand {
       this.log(chalk.green(`Scaled service ${service_name} of component ${component_account_name}/${component_name} deployed to environment ${environment.name} to ${flags.replicas} replicas`));
     } catch(err) {
       const environment_url = `${this.app.config.app_host}/${account.name}/environments/${environment.name}`;
-      this.warn(chalk.yellow(`Did not immediately scale service ${service_name} of component ${component_account_name}/${component_name}.\nIf this was unexpected, check to see that the service is deployed to the environment ${environment.name} at\n${environment_url}.`));
+      this.log(chalk.yellow(`Did not immediately scale service ${service_name} of component ${component_account_name}/${component_name}.\nIf this was unexpected, check to see that the service is deployed to the environment ${environment.name} at\n${environment_url}.`));
     }
-
-    // TODO: warning if service isn't running and couldn't scale immediately
-    // TODO: warning about alpha release
-
     await this.app.api.put(`/environments/${environment.id}`, dto);
     this.log(chalk.green(`Updated scaling settings for service ${service_name} of component ${component_name} for environment ${environment.name}`));
   }
--- test/commands/scale.test.ts
+++ test/commands/scale.test.ts
@@ -1,6 +1,6 @@
 import { expect } from '@oclif/test';
 import sinon from 'sinon';
-import { ComponentVersionSlugUtils } from '../../src';
+import { ComponentVersionSlugUtils, ResourceSlugUtils } from '../../src';
 import Scale from '../../src/commands/scale';
 import { mockArchitectAuth, MOCK_API_HOST } from '../utils/mocks';
 
@@ -33,26 +33,10 @@ const component_version = {
   }
 };
 
-const instance_component_version = {
-  tag: '0.0.2',
-  component: {
-    name: 'echo-instance',
-    component_id: '5678-1234'
-  },
-  config: {
-    services: {
-      app: {},
-      api: {},
-      db: {}
-    },
-    metadata: {
-      instance_name: 'instance-name'
-    }
-  }
-}
-
 const component_version_name = ComponentVersionSlugUtils.build(account.name, component_version.component.name, component_version.tag);
-const component_version_instance_name = ComponentVersionSlugUtils.build(account.name, instance_component_version.component.name, instance_component_version.tag, instance_component_version.config.metadata.instance_name);
+const service_to_scale = 'app';
+const replicas = 5;
+const dto = { replicas, resource_slug: ResourceSlugUtils.build(undefined, component_version.component.name, 'services', service_to_scale) };
 
 describe('Scale', function () {
   describe('Scale services without deploying', function () {
@@ -73,70 +57,37 @@ describe('Scale', function () {
       .stdout({ print })
       .stderr({ print })
 
-    const service_to_scale = 'app';
-    const scaling_settings = {
-      replicas: 5,
-    };
-
-    const replica_scaling_settings: any = { // TODO: type ScalingSettings?
-      scaling_settings: {},
-    };
-    replica_scaling_settings.scaling_settings[component_version_name] = {};
-    replica_scaling_settings.scaling_settings[component_version_name][service_to_scale] = {};
-    replica_scaling_settings.scaling_settings[component_version_name][service_to_scale] = { replicas: scaling_settings.replicas };
+    scale
+      .nock(MOCK_API_HOST, api => api
+        .put(`/environments/${environment.id}/scale`, dto)
+        .reply(200))
+      .nock(MOCK_API_HOST, api => api
+        .put(`/environments/${environment.id}`, dto)
+        .reply(200))
+      .command(['scale', '-e', environment.name, '-a', account.name, `${component_version_name}`, '--service', service_to_scale, '--replicas', replicas.toString()])
+      .it('Sets scaling for service when env exists with env and account flags and updates immediately', ctx => {
+        expect(ctx.stdout).to.contain(`Scaled service ${service_to_scale} of component ${account.name}/${component_version.component.name} deployed to environment ${environment.name} to ${replicas} replicas`);
+        expect(ctx.stdout).to.contain(`Updated scaling settings for service app of component ${component_version.component.name} for environment ${environment.name}`);
+      });
 
     scale
       .nock(MOCK_API_HOST, api => api
-        .put(`/environments/${environment.id}`, replica_scaling_settings)
+        .put(`/environments/${environment.id}/scale`, dto)
+        .reply(404))
+      .nock(MOCK_API_HOST, api => api
+        .put(`/environments/${environment.id}`, dto)
         .reply(200))
-      .command(['scale', '-e', environment.name, '-a', account.name, `${component_version_name}`, '--service', service_to_scale, '--replicas', scaling_settings.replicas.toString()])
-      .it('Sets scaling for service when env exists with env and account flags', ctx => {
-        expect(ctx.stdout).to.contain(`Updated scaling settings for service app of component ${component_version.component.name} in environment ${environment.name}`);
+      .command(['scale', '-e', environment.name, '-a', account.name, `${component_version_name}`, '--service', service_to_scale, '--replicas', replicas.toString()])
+      .it('Sets scaling for service when env exists with env and account flags even if the update cannot take place immediately', ctx => {
+        expect(ctx.stdout).to.contain(`Did not immediately scale service ${service_to_scale} of component ${account.name}/${component_version.component.name}.`);
+        expect(ctx.stdout).to.contain(`Updated scaling settings for service app of component ${component_version.component.name} for environment ${environment.name}`);
       });
 
-    const all_scaling_settings: any = { // TODO: type ScalingSettings?
-      scaling_settings: {},
-    };
-    all_scaling_settings.scaling_settings[component_version_name] = {};
-    all_scaling_settings.scaling_settings[component_version_name][service_to_scale] = {};
-    all_scaling_settings.scaling_settings[component_version_name][service_to_scale] = scaling_settings;
-
     scale
-      .command(['scale', '-e', environment.name, '-a', account.name, `${component_version_name}`, '--service', 'unknown', '--replicas', scaling_settings.replicas.toString()])
+      .command(['scale', '-e', environment.name, '-a', account.name, `${component_version_name}`, '--service', 'unknown', '--replicas', replicas.toString()])
       .catch(err => {
         expect(err.message).to.equal(`Component version ${component_version_name} does not have a service called unknown.`);
       })
       .it(`Fails to set scaling for a service that isn't part of the component`);
-
-    const instance_scaling_settings: any = { // TODO: type ScalingSettings?
-      scaling_settings: {},
-    };
-    instance_scaling_settings.scaling_settings[component_version_instance_name] = {};
-    instance_scaling_settings.scaling_settings[component_version_instance_name][service_to_scale] = {};
-    instance_scaling_settings.scaling_settings[component_version_instance_name][service_to_scale] = { replicas: scaling_settings.replicas };
-
-    mockArchitectAuth
-      .stub(Scale.prototype, 'getCreatePipelineConfirmation', sinon.stub().returns(Promise.resolve(false)))
-      .nock(MOCK_API_HOST, api => api
-        .get(`/accounts/${account.name}`)
-        .reply(200, account))
-      .nock(MOCK_API_HOST, api => api
-        .get(`/accounts/${account.id}/environments/${environment.name}`)
-        .reply(200, environment))
-      .nock(MOCK_API_HOST, api => api
-        .get(`/accounts/${account.name}/components/${instance_component_version.component.name}`)
-        .reply(200, instance_component_version.component))
-      .nock(MOCK_API_HOST, api => api
-        .get(`/components/${instance_component_version.component.component_id}/versions/${instance_component_version.tag}`)
-        .reply(200, instance_component_version))
-      .nock(MOCK_API_HOST, api => api
-        .put(`/environments/${environment.id}`, instance_scaling_settings)
-        .reply(200))
-      .stdout({ print })
-      .stderr({ print })
-      .command(['scale', '-e', environment.name, '-a', account.name, `${component_version_instance_name}`, '--service', service_to_scale, '--replicas', scaling_settings.replicas.toString()])
-      .it('Sets scaling for service with an instance name set', ctx => {
-        expect(ctx.stdout).to.contain(`Updated scaling settings for service app of component ${instance_component_version.component.name} in environment ${environment.name}`);
-      });
   });
 });
