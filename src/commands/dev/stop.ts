5074fea4707e2d2a9308058ce2211b04726e2556
Tyler Aldrich <tyler.aldrich@architect.io>
2022-09-20 16:22:47 -0400

    Use socket communication to stop the running dev process, fallback to compose stop if running detached

--- src/commands/dev/stop.ts
+++ src/commands/dev/stop.ts
@@ -1,10 +1,15 @@
 import { CliUx } from '@oclif/core';
 import chalk from 'chalk';
+import fs from 'fs-extra';
 import inquirer from 'inquirer';
+import net from 'net';
+import path from 'path';
+import { socketPath } from '.';
 import { ArchitectError } from '../..';
 import BaseCommand from "../../base-command";
 import { DockerComposeUtils } from "../../common/docker-compose";
 import { RequiresDocker } from '../../common/docker/helper';
+import LocalPaths from '../../paths';
 
 export default class DevStop extends BaseCommand {
   async auth_required(): Promise<boolean> {
@@ -68,8 +73,19 @@ export default class DevStop extends BaseCommand {
       throw new Error(chalk.red(`No local deployment named '${name}'. Use command 'architect dev:list' to list local deployments.`));
     }
 
-    const compose_file = DockerComposeUtils.buildComposeFilepath(this.app.config.getConfigDir(), name);
-    await DockerComposeUtils.dockerCompose(['-p', name, '-f', compose_file, 'stop']);
+    const socket_path = socketPath(path.join(this.app.config.getConfigDir(), LocalPaths.LOCAL_DEPLOY_PATH, name));
+    if (fs.existsSync(socket_path)) {
+      const socket = net.createConnection(socket_path);
+      socket.write('stop', () => {
+        socket.end();
+      });
+    } else {
+      // If there's no socket, dev is running in detached mode and we can stop it with docker compose without
+      // working about the health check restarting anything.
+      const compose_file = DockerComposeUtils.buildComposeFilepath(this.app.config.getConfigDir(), name);
+      DockerComposeUtils.dockerCompose(['-p', name, '-f', compose_file, 'stop']);
+    }
+
     CliUx.ux.action.start(chalk.blue(`Waiting for ${name} to stop...`));
     const did_stop = await this.waitForEnviromentToStop(name);
     CliUx.ux.action.stop();
--- test/commands/dev/stop.test.ts
+++ test/commands/dev/stop.test.ts
@@ -1,4 +1,6 @@
 import { expect } from '@oclif/test';
+import fs from 'fs-extra';
+import net from 'net';
 import sinon, { SinonSpy } from 'sinon';
 import DevStop from '../../../src/commands/dev/stop';
 import { DockerComposeUtils } from '../../../src/common/docker-compose';
@@ -7,7 +9,7 @@ import { mockArchitectAuth } from '../../utils/mocks';
 describe('dev:stop', () => {
 
   // set to true while working on tests for easier debugging; otherwise oclif/test eats the stdout/stderr
-  const print = false;
+  const print = true;
 
   const env_names = ['test_env'];
 
@@ -22,19 +24,26 @@ describe('dev:stop', () => {
     }
   ]
 
+  const mocked_connection = {
+    write: () => {
+      return;
+    },
+  };
+
   mockArchitectAuth
     .stub(DockerComposeUtils, 'getLocalEnvironments', sinon.stub().returns(env_names))
     .stub(DevStop.prototype, 'waitForEnviromentToStop', sinon.stub().returns(true))
     .stub(DevStop.prototype, 'log', sinon.fake.returns(null))
-    .stub(DockerComposeUtils, 'dockerCompose', sinon.stub().returns(container_states))
+    .stub(fs, 'existsSync', sinon.stub().returns(true))
+    .stub(net, 'createConnection', sinon.stub().returns(mocked_connection))
     .command(['dev:stop', 'test_env'])
     .it('stop a local deployment', ctx => {
       const log_spy = DevStop.prototype.log as SinonSpy;
       expect(log_spy.firstCall.args[0]).to.contain("Successfully stopped local deployment");
 
-      const compose = DockerComposeUtils.dockerCompose as sinon.SinonStub;
-      expect(compose.callCount).to.eq(1);
-      expect(compose.firstCall.firstArg[1]).to.equal('test_env');
+      const connect = net.createConnection as sinon.SinonStub;
+      expect(connect.callCount).to.eq(1);
+      expect(connect.firstCall.firstArg).to.contain('test_env');
     });
 
   mockArchitectAuth
@@ -52,6 +61,7 @@ describe('dev:stop', () => {
     .stub(DockerComposeUtils, 'getLocalEnvironments', sinon.stub().returns(env_names))
     .stub(DevStop.prototype, 'waitForEnviromentToStop', sinon.stub().returns(false))
     .stub(DevStop.prototype, 'log', sinon.fake.returns(null))
+    .stub(net, 'createConnection', sinon.stub().returns(mocked_connection))
     .command(['dev:stop', 'test_env'])
     .catch(err => {
       expect(process.exitCode).eq(1);
