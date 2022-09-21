63b145816c5ba756b306b9b0e91cc7d6b46eeff9
Michael Muesch <mueschm@gmail.com>
2022-09-21 16:50:10 -0400

    feat(dev:list): Add output type to dev list (#711)
    * Add output type to dev list
    
    * Fix invalid json
    
    Co-authored-by: Michael Muesch <michael.muesch@architect.io>

--- src/commands/dev/list.ts
+++ src/commands/dev/list.ts
@@ -1,6 +1,10 @@
+import { Flags } from '@oclif/core';
+import * as util from 'util';
+import { Dictionary } from '../..';
 import BaseCommand from '../../base-command';
 import BaseTable from '../../base-table';
 import { DockerComposeUtils } from '../../common/docker-compose';
+import { DockerInspect } from '../../common/docker-compose/template';
 import { RequiresDocker } from '../../common/docker/helper';
 
 export default class DevList extends BaseCommand {
@@ -8,26 +12,68 @@ export default class DevList extends BaseCommand {
     return false;
   }
 
+  static flags = {
+    format: Flags.string({
+      char: 'f',
+      description: `Format to output data in. Table or JSON`,
+      default: 'table',
+      options: ['TABLE', 'table', 'JSON', 'json'],
+    }),
+  };
+
   static description = 'List all running dev instances.';
   static examples = ['architect dev:list'];
 
-  @RequiresDocker({ compose: true })
-  async run(): Promise<void> {
-    const local_env_map = await  DockerComposeUtils.getLocalEnvironmentContainerMap();
+  outputTable(local_env_map: Dictionary<DockerInspect[]>): void {
     const table = new BaseTable({ head: ['Environment', 'Containers', 'Status'] });
 
     for (const [env_name, containers] of Object.entries(local_env_map)) {
-      const container_names = containers.map(c => {
-        if ('com.docker.compose.service' in c.Config.Labels) {
-          return c.Config.Labels['com.docker.compose.service'];
-        }
-        // Fallback in case compose label isn't present for some reason - this is the image name
-        return c.Name;
-      }).join('\n');
-      const statuses = containers.map(c => c.State.Status).join('\n');
+      const container_names = this.getContainerNames(containers).join('\n');
+      const statuses = this.getContainerStates(containers).join('\n');
       table.push([env_name, container_names, statuses]);
     }
 
     this.log(table.toString());
+  }
+
+  getContainerStates(containers: DockerInspect[]): string[] {
+    return containers.map(c => c.State.Status);
+  }
+
+  getContainerNames(containers: DockerInspect[]): string[] {
+    return containers.map(c => {
+      if ('com.docker.compose.service' in c.Config.Labels) {
+        return c.Config.Labels['com.docker.compose.service'];
+      }
+      // Fallback in case compose label isn't present for some reason - this is the image name
+      return c.Name;
+    });
+  }
+
+  outputJSON(local_env_map: Dictionary<DockerInspect[]>): void {
+    const output: Dictionary<any> = {};
+    for (const [env_name, containers] of Object.entries(local_env_map)) {
+      output[env_name] = {} as Dictionary<any>;
+      const container_names = this.getContainerNames(containers);
+      const statuses = this.getContainerStates(containers);
+      for (let i = 0; i < container_names.length; i++) {
+        output[env_name][container_names[i]] = {
+          status: statuses[i],
+        };
+      }
+    }
+    this.log(JSON.stringify(output, null, 2));
+  }
+
+  @RequiresDocker({ compose: true })
+  async run(): Promise<void> {
+    const { args, flags } = await this.parse(DevList);
+
+    const local_env_map = await DockerComposeUtils.getLocalEnvironmentContainerMap();
+    if (flags.format.toLowerCase() === 'table') {
+      this.outputTable(local_env_map);
+    } else {
+      this.outputJSON(local_env_map);
+    }
   }
 }
