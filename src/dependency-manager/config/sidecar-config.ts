46b7484c478c800d761184d69d300f54bfce928a
TJ Higgins <tj@architect.io>
2022-09-22 14:43:59 -0400

    feat(spec): remove deprecated sidecars field

--- src/dependency-manager/config/sidecar-config.ts
+++
@@ -1,7 +0,0 @@
-import { LivenessProbeConfig } from './common-config';
-import { ResourceConfig } from './resource-config';
-
-export interface SidecarConfig extends Omit<ResourceConfig, 'build'> {
-  enabled: boolean;
-  liveness_probe?: LivenessProbeConfig;
-}
