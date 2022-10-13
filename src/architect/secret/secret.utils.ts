5f63d239d991d4644af2e1eb9ca40dce02348d01
mtgoncalves1 <102971990+mtgoncalves1@users.noreply.github.com>
2022-10-13 15:02:54 -0400

    feat(secrets): Add platform secret functionality
    * Download platform secrets
    
    * Fix download secret tests
    
    * Upload platform secrets
    
    * Add example
    
    * Fix Windows issue
    
    * Provide examples when account is not found and a few refactors
    
    * Fix errors and platform flag
    
    * Fix lint

--- src/architect/secret/secret.utils.ts
+++ src/architect/secret/secret.utils.ts
@@ -1,6 +1,7 @@
 import AppService from '../../app-config/service';
 import Account from '../account/account.entity';
 import { EnvironmentUtils } from '../environment/environment.utils';
+import PlatformUtils from '../platform/platform.utils';
 
 export interface Secret {
   scope: string;
@@ -8,26 +9,34 @@ export interface Secret {
   value: string | number | boolean;
 }
 
+export interface SecretOptions {
+  platform_name?: string;
+  environment_name?: string;
+}
+
 export default class SecretUtils {
-  static async getSecrets(app: AppService, account: Account, environment_name?: string, inherited?: boolean) : Promise<any[]> {
-    let secrets = [];
-    if (!environment_name) {
-      secrets = (await app.api.get(`accounts/${account.id}/secrets/values`)).data;
+  static async getSecrets(app: AppService, account: Account, options?: SecretOptions, inherited?: boolean) : Promise<Secret[]> {
+    let secrets: Secret[] = [];
+    if (options?.environment_name) {
+      const environment = await EnvironmentUtils.getEnvironment(app.api, account, options?.environment_name);
+      secrets = (await app.api.get(`environments/${environment.id}/secrets/values`, { params: { inherited: true } })).data;
+    } else if (options?.platform_name) {
+      const platform = await PlatformUtils.getPlatform(app.api, account, options?.platform_name);
+      secrets = (await app.api.get(`platforms/${platform.id}/secrets/values`, { params: { inherited } })).data;
     } else {
-      const environment = await EnvironmentUtils.getEnvironment(app.api, account, environment_name);
-      if (inherited) {
-        secrets = (await app.api.get(`environments/${environment.id}/secrets/values`, { params: { inherited: true } })).data;
-      } else {
-        secrets = (await app.api.get(`environments/${environment.id}/secrets/values`)).data;
-      }
+      secrets = (await app.api.get(`accounts/${account.id}/secrets/values`)).data;
     }
+
     return secrets;
   }
 
-  static async batchUpdateSecrets(app: AppService, secrets: Secret[], account: Account, environment_name?: string): Promise<void> {
-    if (environment_name) {
-      const environment = await EnvironmentUtils.getEnvironment(app.api, account, environment_name);
+  static async batchUpdateSecrets(app: AppService, secrets: Secret[], account: Account, options?: SecretOptions): Promise<void> {
+    if (options?.environment_name) {
+      const environment = await EnvironmentUtils.getEnvironment(app.api, account, options?.environment_name);
       await app.api.post(`/environments/${environment.id}/secrets/batch`, secrets);
+    } else if (options?.platform_name) {
+      const platform = await PlatformUtils.getPlatform(app.api, account, options?.platform_name);
+      await app.api.post(`/platforms/${platform.id}/secrets/batch`, secrets);
     } else {
       await app.api.post(`/accounts/${account.id}/secrets/batch`, secrets);
     }
