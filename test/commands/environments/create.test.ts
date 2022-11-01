7835dd3b02dc6c6db62c0914b118311907980bc4
Devin Sag <devin.sag@architect.io>
2022-11-01 18:04:46 -0400

    add test case for 409 response warning message

--- test/commands/environments/create.test.ts
+++ test/commands/environments/create.test.ts
@@ -1,25 +1,26 @@
 import { expect } from '@oclif/test';
-import { mockArchitectAuth, MOCK_API_HOST } from '../../utils/mocks';
+import { mockArchitectAuth, MOCK_API_HOST, MOCK_APP_HOST } from '../../utils/mocks';
 
 describe('environment:create', () => {
-
   // set to true while working on tests for easier debugging; otherwise oclif/test eats the stdout/stderr
   const print = false;
 
   const mock_account = {
     id: 'test-account-id',
-    name: 'test-account'
+    name: 'test-account',
   };
 
   const mock_env = {
     id: 'test-env-id',
-    name: 'test-env'
+    name: 'test-env',
   };
 
   const mock_cluster = {
     id: 'test-cluster-id',
-    name: 'test-cluster'
+    name: 'test-cluster',
   };
+
+  const mock_url = `${MOCK_APP_HOST}/${mock_account.name}/environments/${mock_env.name}`;
 
   const create_environment = mockArchitectAuth()
     .nock(MOCK_API_HOST, api => api
@@ -34,10 +35,23 @@ describe('environment:create', () => {
     .stdout({ print })
     .stderr({ print });
 
+  const create_duplicate_environment = mockArchitectAuth()
+    .nock(MOCK_API_HOST, api => api
+      .get(`/accounts/${mock_account.name}`)
+      .reply(200, mock_account))
+    .nock(MOCK_API_HOST, api => api
+      .get(`/accounts/${mock_account.id}/clusters/${mock_cluster.name}`)
+      .reply(200, mock_cluster))
+    .nock(MOCK_API_HOST, api => api
+      .post(`/accounts/${mock_account.id}/environments`)
+      .reply(409))
+    .stdout({ print })
+    .stderr({ print });
+
   create_environment
     .command(['environment:create', mock_env.name, '-a', mock_account.name, '--cluster', mock_cluster.name])
     .it('should create an environment with the cluster flag', ctx => {
-      expect(ctx.stdout).to.contain('Environment created');
+      expect(ctx.stderr).to.contain(`Environment created: ${mock_url}`);
     });
 
   create_environment
@@ -45,14 +59,21 @@ describe('environment:create', () => {
     .command(['environment:create', mock_env.name, '-a', mock_account.name])
     .it('should create an environment with the cluster environment variable', ctx => {
       expect(ctx.stdout).to.contain(`Using cluster from environment variables: ${mock_cluster.name}`);
-      expect(ctx.stdout).to.contain('Environment created');
+      expect(ctx.stderr).to.contain(`Environment created: ${mock_url}`);
+    });
+
+  create_duplicate_environment
+    .env({ ARCHITECT_CLUSTER: mock_cluster.name })
+    .command(['environment:create', mock_env.name, '-a', mock_account.name])
+    .it('should print warning when an environment name already is registerd for an account', ctx => {
+      expect(ctx.stderr).to.contain(`Unable to create new environment '${mock_env.name}'. Environment name already in use for account '${mock_account.name}'`);
     });
 
   create_environment
     .command(['environment:create', mock_env.name, '-a', mock_account.name, '--platform', mock_cluster.name])
     .it('should create an environment with the platform flag, but with a deprecation warning', ctx => {
       expect(ctx.stderr).to.contain('Warning: The "platform" flag has been deprecated. Use "cluster" instead.');
-      expect(ctx.stdout).to.contain('Environment created');
+      expect(ctx.stderr).to.contain(`Environment created: ${mock_url}`);
     });
 
   create_environment
@@ -60,7 +81,7 @@ describe('environment:create', () => {
     .command(['environment:create', mock_env.name, '-a', mock_account.name])
     .it('should create an environment with the platform environment variable', ctx => {
       expect(ctx.stdout).to.contain(`Using cluster from environment variables: ${mock_cluster.name}`);
-      expect(ctx.stdout).to.contain('Environment created');
+      expect(ctx.stderr).to.contain(`Environment created: ${mock_url}`);
     });
 
   mockArchitectAuth()
