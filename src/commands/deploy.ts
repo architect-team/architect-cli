0196302d3b05ebd8e82a1709bb4cb9385caea019
Ryan Cahill <ryan.cahill444@gmail.com>
2022-12-28 16:48:56 -0500

    available urls as set

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -317,7 +317,7 @@ export default class Deploy extends DeployCommand {
       }
     }
 
-    if (available_urls) {
+    if (available_urls.size) {
       this.log('Deployed services are now available at the following URLs:\n');
       for (const url of available_urls) {
         this.log(`\t${url}`);
