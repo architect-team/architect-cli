dcbd624f1197f0d08284b957a07cf028c5bf426d
Devin Sag <devin.sag@architect.io>
2023-01-25 12:06:47 -0500

    update port to match

--- test/integration/hello-world/index.js
+++ test/integration/hello-world/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
