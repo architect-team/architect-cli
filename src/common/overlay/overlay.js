a13cd2636167705deb324cf22d8dd01d9369e259
TJ Higgins <thomashiggins10@gmail.com>
2023-04-26 14:36:14 -0400

    Add tooltips to copy commands

--- src/common/overlay/overlay.js
+++ src/common/overlay/overlay.js
@@ -1,8 +1,25 @@
-function copyToClipboard(text) {
-  navigator.clipboard.writeText(text);
-}
+var Architect = {};
 
-function appendHTML() {
+Architect.copyToClipboard = function (element, text) {
+  navigator.clipboard.writeText(text);
+
+  const tooltipText = element.querySelector('.tooltiptext');
+
+  if (!tooltipText.dataset.text) {
+    tooltipText.dataset.text = tooltipText.innerHTML;
+  }
+
+  tooltipText.innerHTML = 'Copied to clipboard!';
+};
+
+Architect.outFunc = function (element) {
+  const tooltipText = element.querySelector('.tooltiptext');
+  if (tooltipText.dataset.text) {
+    tooltipText.innerHTML = tooltipText.dataset.text;
+  }
+};
+
+Architect.appendHTML = function () {
   // Don't inject the overlay if it already exists
   const overlay = document.querySelector('#architect-overlay');
   if (overlay) {
@@ -12,7 +29,7 @@ function appendHTML() {
   var styles = document.createElement('style');
   styles.innerHTML = `
     #architect-overlay {
-      position: absolute;
+      position: fixed;
       bottom: 0px;
       right: 0px;
       font-family: sans-serif;
@@ -20,7 +37,7 @@ function appendHTML() {
 
     #architect-overlay .dropdown-content {
       display: none;
-      position: absolute;
+      position: fixed;
       min-width: 200px;
       margin-left: -135px;
       transform: translate(0%, -100%) !important;
@@ -36,16 +53,8 @@ function appendHTML() {
       display: block;
     }
 
-    #architect-overlay .dropdown-content a:first-child {
-      border-top-left-radius: 5px;
-    }
-
-    #architect-overlay .dropdown-content a:last-child {
-      border-bottom-left-radius: 5px;
-    }
-
     #architect-overlay .dropdown-content a:hover {
-      filter: brightness(125%);
+      background-color: #368798;
     }
 
     #architect-overlay:hover .dropdown-content {
@@ -73,6 +82,41 @@ function appendHTML() {
      #architect-overlay:hover .dropdown-button svg {
       filter: brightness(125%);
     }
+
+    .tooltip {
+      position: relative;
+    }
+
+    .tooltip .tooltiptext {
+      visibility: hidden;
+      width: 260px;
+      background-color: #555;
+      color: #fff;
+      text-align: center;
+      border-radius: 6px;
+      padding: 8px 0;
+
+      /* Position the tooltip */
+      position: absolute;
+      z-index: 1;
+      top: 5px;
+      right: 105%;
+    }
+
+    .tooltip:hover .tooltiptext {
+      visibility: visible;
+    }
+
+    .tooltip .tooltiptext::after {
+      content: " ";
+      position: absolute;
+      top: 50%;
+      left: 100%; /* To the right of the tooltip */
+      margin-top: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: transparent transparent transparent #555;
+    }
   `;
   document.body.append(styles);
 
@@ -80,20 +124,38 @@ function appendHTML() {
   wrapper.id = 'architect-overlay';
   wrapper.innerHTML = `
     <div class="dropdown-content">
-      <a href="#" onclick="copyToClipboard('architect logs')">Logs (CLI)</a>
-      <a href="#" onclick="copyToClipboard('architect exec')">Exec (CLI)</a>
-      <a href="#" onclick="copyToClipboard('architect dev:restart')">Restart (CLI)</a>
-      <a href="https://docs.architect.io" target="_blank">Docs</a>
-      <a href="https://www.architect.io" target="_blank">Deployed with Architect</a>
+      <div class="tooltip">
+        <a href="#" onclick="Architect.copyToClipboard(this, 'architect logs')" onmouseout="Architect.outFunc(this)" style="border-top-left-radius: 5px;">
+          <span class="tooltiptext">View the logs from the CLI</span>
+          Logs
+        </a>
+      </div>
+
+      <div class="tooltip">
+        <a href="#" onclick="Architect.copyToClipboard(this, 'architect exec')" onmouseout="Architect.outFunc(this)">
+          <span class="tooltiptext">Execute a command from the CLI</span>
+          Exec
+        </a>
+
+      </div>
+
+      <div class="tooltip">
+        <a href="#" onclick="Architect.copyToClipboard(this, 'architect dev:restart')" onmouseout="Architect.outFunc(this)">
+          <span class="tooltiptext">Restart this service from the CLI</span>
+          Restart
+        </a>
+      </div>
+
+      <a href="https://docs.architect.io" target="_blank">Documentation</a>
+      <a href="https://www.architect.io" target="_blank" style="border-bottom-left-radius: 5px;">Deployed with Architect</a>
     </div>
     <div class="dropdown-button">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 158.5882 111.9446">
         <path d="M118.3551,37.5464V0H40.581L0,74.3982H40.2332v37.5464h77.7742l40.5808-74.3982ZM77.7829,111.9273V74.3949H40.2357L80.8054.0173V37.55h37.5471Z"/>
       </svg>
     </div>
-
   `;
   document.body.append(wrapper);
-}
+};
 
-appendHTML();
+Architect.appendHTML();
