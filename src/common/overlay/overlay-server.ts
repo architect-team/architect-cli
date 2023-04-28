adfcaa34e5b14f08c720f1015677cfb8370e75cc
TJ Higgins <thomashiggins10@gmail.com>
2023-04-28 11:21:44 -0400

    Try catch error in server

--- src/common/overlay/overlay-server.ts
+++ src/common/overlay/overlay-server.ts
@@ -9,12 +9,19 @@ export class OverlayServer {
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-      // eslint-disable-next-line unicorn/prefer-module
-      const file_path = path.join(__dirname, '../../static/overlay.js');
-      const file = fs.readFileSync(file_path);
-      res.writeHead(200, { 'Content-Type': 'text/javascript' });
-      res.end(file);
+      try {
+        // eslint-disable-next-line unicorn/prefer-module
+        const file_path = path.join(__dirname, '../../static/overlay.js');
+        const file = fs.readFileSync(file_path);
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.end(file);
+      } catch (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server Error');
+      }
     });
+
+    server.on('error', err => console.log(err));
 
     server.listen(port);
   }
